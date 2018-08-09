2e2f90d5e28e2462c34d59ff4eb379a9ea6e9b95
Pierre Wielders <pierre@wielders.net>
2018-08-09 16:01:28 +0200

    [RESOLUTION] Add the brandnew 4K screen resolutions..

--- Source/interfaces/IComposition.h
+++ Source/interfaces/IComposition.h
@@ -18,7 +18,9 @@ namespace Exchange {
             ScreenResolution_1080p24Hz = 5,
             ScreenResolution_1080i50Hz = 6,
             ScreenResolution_1080p50Hz = 7,
-            ScreenResolution_1080p60Hz = 8
+            ScreenResolution_1080p60Hz = 8,
+            ScreenResolution_2160p50Hz = 9,
+            ScreenResolution_2160p60Hz = 10
         };
 
         struct IClient : virtual public Core::IUnknown {
