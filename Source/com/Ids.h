kraj/WPEFramework

[OCDM] EXTENSION IDs added

Source/com/Ids.h
@@ -12,8 +12,10 @@ enum IDS {
 
     ID_ACCESSOROCDM = 0x00000010,
     ID_ACCESSOROCDM_NOTIFICATION = 0x00000011,
-    ID_SESSION = 0x00000012,
-    ID_SESSION_CALLBACK = 0x00000013,
+    ID_ACCESSOROCDM_EXTENSION = 0x00000012,
+    ID_SESSION = 0x00000013,
+    ID_SESSION_CALLBACK = 0x00000014,
+    ID_SESSION_EXTENSION = 0x00000015,
 
     ID_PLUGIN = 0x00000020,
     ID_PLUGIN_NOTIFICATION = 0x00000021,

rdkcentral/Thunder

Source/com/Ids.h
@@ -12,9 +12,11 @@ namespace RPC {
         ID_VALUEITERATOR = 0x00000006,
 
         ID_ACCESSOROCDM = 0x00000010,
-        ID_ACCESSOROCDM_NOTIFICATION = 0x00000011,
-        ID_SESSION = 0x00000012,
-        ID_SESSION_CALLBACK = 0x00000013,
+        ID_ACCESSOROCDM_EXTENSION = 0x00000011,
+        ID_ACCESSOROCDM_NOTIFICATION = 0x00000012,
+        ID_SESSION = 0x00000013,
+        ID_SESSION_CALLBACK = 0x00000014,
+        ID_SESSION_EXTENSION = 0x00000015,
 
         ID_PLUGIN = 0x00000020,
         ID_PLUGIN_NOTIFICATION = 0x00000021,