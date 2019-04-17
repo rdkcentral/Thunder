58ef727e5ba90b1918fc3aba6234253d04fa3927
Pierre Wielders <pierre@wielders.net>
2019-04-17 19:36:18 +0200

    Revert "Synchronize config.json location (C++ & cmake)"
    This reverts commit d5fba16a878921db53aaa2c81a95d9e4dc3d4d50.

--- Source/WPEFramework/PluginServer.cpp
+++ Source/WPEFramework/PluginServer.cpp
@@ -40,7 +40,7 @@ namespace PluginHost
 #ifdef __WIN32__
     /* static */ const TCHAR* Server::ConfigFile = _T("C:\\Projects\\PluginHost.json");
 #else
-    /* static */ const TCHAR* Server::ConfigFile = _T(WPEFRAMEWORK_CONFIG_FILE);
+    /* static */ const TCHAR* Server::ConfigFile = _T("/etc/" EXPAND_AND_QUOTE(NAMESPACE) "/config.json");
 #endif
 
     /* static */ const TCHAR* Server::PluginOverrideFile = _T("PluginHost/override.json");
