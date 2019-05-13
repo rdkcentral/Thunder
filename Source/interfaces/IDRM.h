5b4324ddc8b66404f277cc31bbd3bbd4a6ad8c97
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2019-05-13 10:59:39 +0200

    Constify OnKeyMessage() f_pszUrl parameter
    Helps to fix errors on OCDM-Playready like;
     error: ISO C++ forbids converting a string constant to ‘char*’
             m_piCallback->OnKeyMessage((const uint8_t *) "", 0, "");

--- Source/interfaces/IDRM.h
+++ Source/interfaces/IDRM.h
@@ -207,7 +207,7 @@ public:
     virtual void OnKeyMessage(
         const uint8_t* f_pbKeyMessage, //__in_bcount(f_cbKeyMessage)
         uint32_t f_cbKeyMessage, //__in
-        char* f_pszUrl)
+        const char* f_pszUrl)
         = 0; //__in_z_opt
 
     // Event fired when MediaKeySession has found a usable key.
