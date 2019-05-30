9f9034b1a66ea9c4ba4865219a6b7e00206132cb
sebaszm <s.szczepaniak@metrological.com>
2019-05-30 10:51:07 +0200

    [Interfaces] JSON-RPC, TimeSync to use properties

--- Source/interfaces/json/JsonData_TimeSync.h
+++ Source/interfaces/json/JsonData_TimeSync.h
@@ -17,37 +17,22 @@ namespace JsonData {
         // Method params/result classes
         //
 
-        class SetParamsData : public Core::JSON::Container {
+        class SynctimeParamsData : public Core::JSON::Container {
         public:
-            SetParamsData()
-                : Core::JSON::Container()
-            {
-                Add(_T("time"), &Time);
-            }
-
-            SetParamsData(const SetParamsData&) = delete;
-            SetParamsData& operator=(const SetParamsData&) = delete;
-
-        public:
-            Core::JSON::String Time; // New system time (in ISO8601 format); if this parameter is omitted then the *time* subsystem will still become activated but without setting the time (thereby notifying the framework that the time has been set externally)
-        }; // class SetParamsData
-
-        class TimeResultData : public Core::JSON::Container {
-        public:
-            TimeResultData()
+            SynctimeParamsData()
                 : Core::JSON::Container()
             {
                 Add(_T("time"), &Time);
                 Add(_T("source"), &Source);
             }
 
-            TimeResultData(const TimeResultData&) = delete;
-            TimeResultData& operator=(const TimeResultData&) = delete;
+            SynctimeParamsData(const SynctimeParamsData&) = delete;
+            SynctimeParamsData& operator=(const SynctimeParamsData&) = delete;
 
         public:
             Core::JSON::String Time; // Synchronized time (in ISO8601 format); empty string if the time has never been synchronized
             Core::JSON::String Source; // The synchronization source e.g. an NTP server
-        }; // class TimeResultData
+        }; // class SynctimeParamsData
 
     } // namespace TimeSync
 
