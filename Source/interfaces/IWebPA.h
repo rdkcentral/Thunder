05a9c4efc2fc8cb8b932bd53b796a34a79cbaef1
HaseenaSainul <h.sainul@metrological.com>
2019-06-25 18:13:09 +0530

    [WebPA] interface changes

--- Source/interfaces/IWebPA.h
+++ Source/interfaces/IWebPA.h
@@ -6,15 +6,18 @@
 namespace WPEFramework {
 namespace Exchange {
 
-    struct IWebPAService : virtual public Core::IUnknown {
+    struct IWebPA : virtual public Core::IUnknown {
         enum { ID = ID_WEBPA };
-        virtual uint32_t Configure(PluginHost::IShell*) = 0;
-        virtual void Launch() = 0;
-    };
-    struct IWebPAClient : virtual public Core::IUnknown {
-        enum { ID = ID_WEBPA_CLIENT };
-        virtual uint32_t Configure(PluginHost::IShell*) = 0;
-        virtual void Launch() = 0;
+        virtual uint32_t Initialize(PluginHost::IShell*) = 0;
+        virtual void Deinitialize(PluginHost::IShell*) = 0;
+
+        struct IWebPAClient : virtual public Core::IUnknown {
+            enum { ID = ID_WEBPA_CLIENT };
+            virtual uint32_t Configure(PluginHost::IShell*) = 0;
+            virtual void Launch() = 0;
+        };
+
+        virtual IWebPAClient* Client(const string& name) = 0;
     };
 }
 }
