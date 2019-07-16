136defa8e5714b9e21f3a2c2ef7ab73594caf2bb
Suman Rapolu <suman.rapolu@soctronics.com>
2019-07-16 19:20:07 +0530

    [DropbearServerPlugin] Implementation for session management support through WPEFramework

--- Source/interfaces/json/JsonData_DropbearServer.h
+++ Source/interfaces/json/JsonData_DropbearServer.h
@@ -22,6 +22,8 @@ namespace JsonData {
             StartserviceParamsData()
                 : Core::JSON::Container()
             {
+                Add(_T("hostkeys"), &HostKeys);
+                Add(_T("portflag"), &PortFlag);
                 Add(_T("port"), &Port);
             }
 
@@ -29,8 +31,63 @@ namespace JsonData {
             StartserviceParamsData& operator=(const StartserviceParamsData&) = delete;
 
         public:
-            Core::JSON::String Port; // USB device to reset
-        }; // class UsbresetParamsData
+            Core::JSON::String Port; // Port on which DHCP service shall run
+            Core::JSON::String PortFlag; // Flag to enable user defined port number to use
+            Core::JSON::String HostKeys; // Falg to run the DHCP service in background mode
+        }; // class StartserviceParamsData
+
+        class CloseclientsessionParamsData : public Core::JSON::Container {
+        public:
+            CloseclientsessionParamsData()
+                : Core::JSON::Container()
+            {
+                Add(_T("pid"), &Pid);
+            }
+
+            CloseclientsessionParamsData(const CloseclientsessionParamsData&) = delete;
+            CloseclientsessionParamsData& operator=(const CloseclientsessionParamsData&) = delete;
+
+        public:
+            Core::JSON::String Pid; // Port on which DHCP service shall run
+        }; // class CloseclientsessionParamsData
+
+        class SessioninfoResultData : public Core::JSON::Container {
+        public:
+            SessioninfoResultData()
+                : Core::JSON::Container()
+            {
+		Init();
+            }
+
+            SessioninfoResultData(const SessioninfoResultData& other)
+                : Core::JSON::Container()
+                , Pid(other.Pid)
+                , IpAddress(other.IpAddress)
+                , TimeStamp(other.TimeStamp)
+            {
+                Init();
+            }
+
+            SessioninfoResultData& operator=(const SessioninfoResultData& rhs)
+            {
+                Pid = rhs.Pid;
+                IpAdress = rhs.IpAddress;
+                TimeStamp = rhs.TimeStamp;
+                return (*this);
+            }
+
+            void Init()
+            {
+                Add(_T("pid"), &Pid);
+                Add(_T("ipaddress"), &IpAddress);
+                Add(_T("timestamp"), &TimeStamp);
+            }
+
+        public:
+            Core::JSON::String Pid; // Port on which DHCP service shall run
+            Core::JSON::String IpAddress; // Port on which DHCP service shall run
+            Core::JSON::String TimeStamp; // Port on which DHCP service shall run
+        }; // class SessioninfoResultData
 
     } // namespace DropbearServer
 
