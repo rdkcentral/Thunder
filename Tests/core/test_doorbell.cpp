fa846f8e70481d49ef596e1c601a19309c7c7411
aswaunni <aswathi.u@tataelxsi.co.in>
2019-08-08 08:43:06 +0000

    [Tests] Added DoorBell test.

--- Tests/core/test_doorbell.cpp
+++ Tests/core/test_doorbell.cpp
@@ -3,36 +3,33 @@
 #include <gtest/gtest.h>
 #include <core/core.h>
 
-namespace WPEFramework {
-namespace Tests {
+using namespace WPEFramework;
 
-    string g_fileName = "/tmp/doorbell01";
+string g_fileName = "/tmp/doorbell01";
 
-    TEST(Core_DoorBell, simpleSet)
-    {
-        IPTestAdministrator::OtherSideMain otherSide = [](IPTestAdministrator & testAdmin) {
-            Core::DoorBell doorBell(g_fileName.c_str());
-            if (doorBell.Wait(Core::infinite) == Core::ERROR_NONE) {
-                doorBell.Acknowledge();
-                testAdmin.Sync("First ring");
-            }
-
-            if (doorBell.Wait(Core::infinite) == Core::ERROR_NONE) {
-                doorBell.Acknowledge();
-                testAdmin.Sync("Second ring");
-            }
-            doorBell.Relinquish();
-        };
-
-        IPTestAdministrator testAdmin(otherSide);
-        {
-            usleep(1000);
-            Core::DoorBell doorBell(g_fileName.c_str());
-            doorBell.Ring();
+TEST(Core_DoorBell, simpleSet)
+{
+    IPTestAdministrator::OtherSideMain otherSide = [](IPTestAdministrator & testAdmin) {
+        Core::DoorBell doorBell(g_fileName.c_str());
+        if (doorBell.Wait(Core::infinite) == Core::ERROR_NONE) {
+            doorBell.Acknowledge();
             testAdmin.Sync("First ring");
-            doorBell.Ring();
+        }
+
+        if (doorBell.Wait(Core::infinite) == Core::ERROR_NONE) {
+            doorBell.Acknowledge();
             testAdmin.Sync("Second ring");
         }
+        doorBell.Relinquish();
+    };
+
+    IPTestAdministrator testAdmin(otherSide);
+    {
+        sleep(2);
+        Core::DoorBell doorBell(g_fileName.c_str());
+        doorBell.Ring();
+        testAdmin.Sync("First ring");
+        doorBell.Ring();
+        testAdmin.Sync("Second ring");
     }
-}
 }
