4fb6edbc1ff4c0ca9a2ea01e4856c4b4d5f24a5d
anjalirajan <anjali.r@tataelxsi.co.in>
2019-08-14 08:34:47 +0000

    [Tests] Added test cases for TextFragment.

--- Tests/core/test_textfragment.cpp
+++ Tests/core/test_textfragment.cpp
@@ -1,7 +1,8 @@
 #include "../IPTestAdministrator.h"
 
 #include <gtest/gtest.h>
-#include <core/core.h>
+#include <core/TextFragment.h>
+#include <core/SystemInfo.h>
 
 using namespace WPEFramework;
 using namespace WPEFramework::Core;
@@ -16,18 +17,15 @@ TEST(test_textfragment, simple_textfragement)
     index.Next();
     index.Next();
 
-    EXPECT_STREQ(index.Remainder().Text().c_str(), _T("test")) << "The remainder string is not test";
-    EXPECT_STREQ(index.Remainder().Data(), _T("test")) << "The remainder string is not test";
-    EXPECT_STREQ(index.Current().Text().c_str(), _T("test")) << "The current string is not test";
-    EXPECT_EQ(index.Remainder().Length(), 4) << "The length of the string is not 4.";
-    EXPECT_FALSE(index.Remainder().IsEmpty());
+    ASSERT_STREQ(index.Remainder().Text().c_str(), _T("test")) << "The remainder string is not test";
+    ASSERT_STREQ(index.Remainder().Data(), _T("test")) << "The remainder string is not test";
+    ASSERT_STREQ(index.Current().Text().c_str(), _T("test")) << "The current string is not test";
+    ASSERT_EQ(index.Remainder().Length(), 4) << "The length of the string is not 4.";
+    ASSERT_FALSE(index.Remainder().IsEmpty());
     
     Core::TextFragment textFragment();
     const TCHAR buffer_new[] = "/Service/testing/test";
     Core::TextFragment textFragment1(buffer_new);
     Core::TextFragment textFragment2(buffer_new,21);
     Core::TextFragment textFragment3(buffer_new,16,5);
-
-    char delimiter[] = {'/'};
-    EXPECT_EQ(textFragment1.ForwardFind(delimiter),0);
 }
