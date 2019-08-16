3e8f221ad314b0c23efd916fa6b885b88cad819f
Sander van der Maar <sandervandermaar@gmail.com>
2019-08-16 12:04:03 +0200

    [CORE] Cleans up destruction of Singletons.
    - Adds BUILD_TESTS flag, allows for code needed during testing, but would be slower in production.
    - Reorders termination of MonitorWorker thread.
    - Sorts out destruction of Singleton: still report existence during clean-up, to prevent new creation.
    - Tests can now use Singleton::Dispose() to bring system back to initial state.
    - When building debug, use no optimization. Even "-Og" optimizes out this-pointers.

--- Source/core/ResourceMonitor.cpp
+++ Source/core/ResourceMonitor.cpp
@@ -7,8 +7,16 @@ namespace Core {
 
     /* static */ ResourceMonitor& ResourceMonitor::Instance()
     {
+        // Tests build/destroy the ResourceMonitor for each test. In production the
+        // ResourceMonitor only gets built/destroyed once. The static variable "_instance"
+        // makes this method about 5% faster. So here test code and production code are
+        // different, but its impact has been thoroughly researched.
+#ifdef BUILD_TESTS
+        return SingletonType<ResourceMonitor>::Instance();
+#else
         static ResourceMonitor& _instance = SingletonType<ResourceMonitor>::Instance();
         return (_instance);
+#endif
     }
 }
 } // namespace WPEFramework::Core
