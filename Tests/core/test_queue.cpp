094d58810f1c7255f6a675c16358e376293b9ee8
anjalirajan <anjali.r@tataelxsi.co.in>
2019-08-21 09:04:03 +0000

    [Tests] Improved the coverage of test Quque.

--- Tests/core/test_queue.cpp
+++ Tests/core/test_queue.cpp
@@ -10,17 +10,13 @@ using namespace WPEFramework::Core;
 TEST(test_queue, simple_queue)
 {
     QueueType<int> obj1(20);
-    bool ins= obj1.Insert(20,300);
-    ASSERT_TRUE(ins);
-    bool ins1= obj1.Insert(30,300);
-    ASSERT_TRUE(ins1);
-    bool post= obj1.Post(20);
-    ASSERT_TRUE(post);
-    bool remove= obj1.Remove(20);
-    ASSERT_TRUE(remove);
+    ASSERT_TRUE(obj1.Insert(20,300));
+    ASSERT_TRUE(obj1.Insert(30,300));
+    ASSERT_TRUE(obj1.Post(20));
+    ASSERT_TRUE(obj1.Remove(20));
     int a_Result = 30;
-    bool extract= obj1.Extract(a_Result,300);
-    ASSERT_TRUE(extract);
+    ASSERT_TRUE(obj1.Extract(a_Result,300));
+    ASSERT_EQ(obj1.Length(),1);
     obj1.Enable();
     obj1.Disable();
     obj1.Flush();
