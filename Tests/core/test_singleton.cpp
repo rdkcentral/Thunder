3789a28741949f9828a77602f2398ec63c0dce51
anjalirajan <anjali.r@tataelxsi.co.in>
2019-08-29 05:21:32 +0000

    [Tests] Added testcases for Singleton.

--- Tests/core/test_singleton.cpp
+++ Tests/core/test_singleton.cpp
@@ -40,13 +40,12 @@ TEST(test_singleton, simple_singleton)
 {
     static SingletonTypeOne& object1 = SingletonType<SingletonTypeOne>::Instance();
     static SingletonTypeOne& object_sample = SingletonType<SingletonTypeOne>::Instance();
-    EXPECT_EQ(&object1,&object_sample);
+    ASSERT_EQ(&object1,&object_sample);
     static SingletonTypeTwo& object2 = SingletonType<SingletonTypeTwo>::Instance("SingletonTypeTwo");
     static SingletonTypeThree& object3 = SingletonType<SingletonTypeThree>::Instance("SingletonTypeThree","SingletonTypeThree");
     SingletonType<SingletonTypeTwo>* x = (SingletonType<SingletonTypeTwo>*)&object2;
-    EXPECT_STREQ(x->ImplementationName().c_str(),"SingletonTypeTwo");
+    ASSERT_STREQ(x->ImplementationName().c_str(),"SingletonTypeTwo");
     SingletonType<SingletonTypeThree>* y = (SingletonType<SingletonTypeThree>*)&object3;
-    EXPECT_STREQ(y->ImplementationName().c_str(),"SingletonTypeThree");
+    ASSERT_STREQ(y->ImplementationName().c_str(),"SingletonTypeThree");
     Singleton::Dispose();
 }
-
