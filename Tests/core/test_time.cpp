426e527a81feff4af6ee17d06c680572c023e74c
anjalirajan <anjali.r@tataelxsi.co.in>
2019-09-16 06:28:37 +0000

    [Tests] Updated test case of time test.

--- Tests/core/test_time.cpp
+++ Tests/core/test_time.cpp
@@ -30,16 +30,18 @@ TEST(Core_Time, timedetails)
     time.NTPTime();
     time.DifferenceFromGMTSeconds();
     time.FromRFC1123(timeString); 
+    
+    Time::Now().ToRFC1123();
     time.Add (2000);
     SystemInfo::Instance().SetTime(time);
     Time::Now().ToRFC1123();
 
     time.Sub (2000);
     SystemInfo::Instance().SetTime(time);
+    Time::Now().ToRFC1123();
 
     Time t1 =  Time(2018,7,23,11,30,23,21,true);
     Time t2 =  Time(2018,7,23,11,30,23,21,false);
-    //t1.(timeString);    
     struct tm setTime;
     ::memcpy(&setTime, &(time.Handle()), sizeof (setTime));
 }
@@ -69,38 +71,67 @@ TEST(Core_Time, FromRFC1123)
     Time t1 =  Time(2018,1,23,11,30,23,21,false);
     std::string timeString;
     t1.ToString(timeString);
-    t1.FromRFC1123(timeString); 
+    t1.FromRFC1123(timeString);
+    EXPECT_STREQ(timeString.c_str() ,_T("Tue, 23 Jan 2018 11:30:23 "));
+ 
     t1 =  Time(2018,2,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Fri, 23 Feb 2018 11:30:23 "));
+
     t1 =  Time(2018,3,23,11,30,23,21,true);
     t1.ToString(timeString);
-    t1.FromRFC1123(timeString); 
+    t1.FromRFC1123(timeString);
+    EXPECT_STREQ(timeString.c_str() ,_T("Fri, 23 Mar 2018 11:30:23 "));
+
     t1 =  Time(2018,4,23,11,30,23,21,true);
     t1.ToString(timeString);
-    t1.FromRFC1123(timeString); 
+    t1.FromRFC1123(timeString);
+    EXPECT_STREQ(timeString.c_str() ,_T("Mon, 23 Apr 2018 11:30:23 "));
+
     t1 =  Time(2018,5,23,11,30,23,21,true);
     t1.ToString(timeString);
-    t1.FromRFC1123(timeString); 
+    t1.FromRFC1123(timeString);
+    EXPECT_STREQ(timeString.c_str() ,_T("Wed, 23 May 2018 11:30:23 "));
+     
     t1 =  Time(2018,6,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Sat, 23 Jun 2018 11:30:23 "));
+
+    t1 =  Time(2018,7,23,11,30,23,21,true);
+    t1.ToString(timeString);
+    t1.FromRFC1123(timeString);
+    EXPECT_STREQ(timeString.c_str() ,_T("Mon, 23 Jul 2018 11:30:23 ")); 
+
     t1 =  Time(2018,8,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Thu, 23 Aug 2018 11:30:23 "));
+
     t1 =  Time(2018,9,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Sun, 23 Sep 2018 11:30:23 "));
+    
     t1 =  Time(2018,10,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Tue, 23 Oct 2018 11:30:23 "));
+    
     t1 =  Time(2018,11,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Fri, 23 Nov 2018 11:30:23 "));
+
     t1 =  Time(2018,12,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Sun, 23 Dec 2018 11:30:23 "));
+ #if 0   
     t1 =  Time(2018,13,23,11,30,23,21,true);
     t1.ToString(timeString);
     t1.FromRFC1123(timeString); 
+    EXPECT_STREQ(timeString.c_str() ,_T("Wed, 23 Jan 2019 11:30:23 "));
+#endif
 }
