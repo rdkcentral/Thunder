95e821bb5bfb45eb4a91b9fb3671c72a0bd32d63
anjalirajan <anjali.r@tataelxsi.co.in>
2019-09-25 17:08:35 +0530

    [TESTS] Updated Queue test.

--- Tests/core/test_queue.cpp
+++ Tests/core/test_queue.cpp
@@ -1,7 +1,7 @@
 #include "../IPTestAdministrator.h"
 
 #include <gtest/gtest.h>
-#include <core/Queue.h>
+#include <core/core.h>
 
 using namespace WPEFramework;
 using namespace WPEFramework::Core;
@@ -10,13 +10,13 @@ using namespace WPEFramework::Core;
 TEST(test_queue, simple_queue)
 {
     QueueType<int> obj1(20);
-    ASSERT_TRUE(obj1.Insert(20,300));
-    ASSERT_TRUE(obj1.Insert(30,300));
-    ASSERT_TRUE(obj1.Post(20));
-    ASSERT_TRUE(obj1.Remove(20));
+    EXPECT_TRUE(obj1.Insert(20,300));
+    EXPECT_TRUE(obj1.Insert(30,300));
+    EXPECT_TRUE(obj1.Post(20));
+    EXPECT_TRUE(obj1.Remove(20));
     int a_Result = 30;
-    ASSERT_TRUE(obj1.Extract(a_Result,300));
-    ASSERT_EQ(obj1.Length(),1);
+    EXPECT_TRUE(obj1.Extract(a_Result,300));
+    EXPECT_EQ(obj1.Length(),1);
     obj1.Enable();
     obj1.Disable();
     obj1.Flush();
