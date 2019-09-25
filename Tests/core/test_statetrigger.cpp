c6a535deba58d71d7cd299520b04e924b7d931ab
anjalirajan <anjali.r@tataelxsi.co.in>
2019-09-25 16:11:13 +0530

    [TESTS] Increased coverage of StateTrigger.

--- Tests/core/test_statetrigger.cpp
+++ Tests/core/test_statetrigger.cpp
@@ -1,30 +1,32 @@
 #include "../IPTestAdministrator.h"
 
 #include <gtest/gtest.h>
-#include <core/StateTrigger.h>
+#include <core/core.h>
 
 using namespace WPEFramework;
 using namespace WPEFramework::Core;
 
-time_t g_time =time(NULL)+1;
-
+enum testState {
+    TEST_INIT = 0x00,
+    TEST_MESSAGE = 0x01,
+    TEST_READY = 0x02,
+    TEST_ERROR = 0x03,
+};
 TEST(test_statetrigger, simple_statetrigger)
 {
-    enum testState {
-        TEST_INIT = 0x00,
-        TEST_MESSAGE = 0x01,
-        TEST_READY = 0x02,
-        TEST_ERROR = 0x03,
-    };
-
     WPEFramework::Core::StateTrigger<testState> state(TEST_READY);
-    ASSERT_EQ(state.GetState(),TEST_READY) << "State not equal to TEST_READY.";
-    state.SetState(TEST_MESSAGE);
-    ASSERT_NE(state.GetState(),TEST_READY) << "State is TEST_MESSAGE.";
-    ASSERT_TRUE(state.GetState() == TEST_MESSAGE) << "State is not TEST_MESSAGE";
-    ASSERT_FALSE(state.GetState() != TEST_MESSAGE) << "State is TEST_MESSAGE";
-    state = TEST_READY;
-    state.WaitState(TEST_READY,g_time);
 
-    state.WaitStateClear(TEST_INIT,g_time+5);
+    EXPECT_EQ(state.GetState(),TEST_READY) << "State not equal to TEST_READY.";
+    state.SetState(TEST_MESSAGE);
+    EXPECT_NE(state.GetState(),TEST_READY) << "State is TEST_MESSAGE.";
+    EXPECT_TRUE(state.GetState() == TEST_MESSAGE) << "State is not TEST_MESSAGE";
+    EXPECT_FALSE(state.GetState() != TEST_MESSAGE) << "State is TEST_MESSAGE";
+    state = TEST_READY;
+    uint64_t timeOut(Core::Time::Now().Add(5).Ticks());
+    uint64_t now(Core::Time::Now().Ticks());
+    state.WaitState(TEST_INIT,static_cast<uint32_t>((timeOut - now) / Core::Time::TicksPerMillisecond));
+    
+    timeOut = Core::Time::Now().Add(3).Ticks();
+    now = Core::Time::Now().Ticks();
+    state.WaitStateClear(TEST_READY,static_cast<uint32_t>((timeOut - now) / Core::Time::TicksPerMillisecond));
 }
