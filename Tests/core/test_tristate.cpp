45b1891b63d424347c2b40cd8bd04ebe6038f5da
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-01 17:42:06 +0530

    [TESTS] Added Tristate test.

---
+++ Tests/core/test_tristate.cpp
@@ -0,0 +1,26 @@
+#include "../IPTestAdministrator.h"
+
+#include <gtest/gtest.h>
+#include <core/core.h>
+
+using namespace WPEFramework;
+using namespace WPEFramework::Core;
+
+TEST(test_TextReader, simple_TextReader)
+{
+    TriState();
+    TriState tristate1("F");
+    TriState tristate2("T");
+    TriState tristate3("True");
+    TriState tristate4("False");
+    TriState tristate5(tristate1);
+    TriState tristate6 =tristate1;
+    TriState tristate7(TriState::EnumState::True);
+
+    EXPECT_EQ(tristate3.Get(),TriState::EnumState::True);
+    tristate3.Set(false);
+    EXPECT_EQ(tristate3.Get(),TriState::EnumState::False);
+    tristate3.Set(TriState::EnumState::Unknown);
+    EXPECT_EQ(tristate3.Get(),TriState::EnumState::Unknown);
+}
+
