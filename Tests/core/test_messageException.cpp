8bb7cac6f26bb7a30550e1005dd9267cffda8229
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-10 12:28:59 +0530

    [TESTS] Added MessageException test.

---
+++ Tests/core/test_messageException.cpp
@@ -0,0 +1,19 @@
+#include "../IPTestAdministrator.h"
+
+#include <gtest/gtest.h>
+#include <core/core.h>
+
+using namespace WPEFramework;
+using namespace WPEFramework::Core;
+
+TEST(test_messageException, simple_messageException)
+{
+    std::string msg = "Testing the message exception.";
+    MessageException exception(msg.c_str(),false);
+    EXPECT_STREQ(exception.Message(),msg.c_str());
+    
+    MessageException exception1(msg.c_str(),true);
+    char buffer[50];
+    sprintf(buffer,"%s: Success",msg.c_str());
+    EXPECT_STREQ(exception1.Message(),buffer);
+}
