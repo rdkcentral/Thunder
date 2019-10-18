3086fbc7f68a7d425e0c2509300626bfc0a32f07
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-18 12:05:42 +0530

    [Tests] Updated Parser test..

--- Tests/core/test_parser.cpp
+++ Tests/core/test_parser.cpp
@@ -6,38 +6,6 @@
 using namespace WPEFramework;
 using namespace WPEFramework::Core;
 
-class Deserializer {
-private:
-    typedef ParserType<TerminatorCarriageReturnLineFeed, Deserializer> Parser;
-public :
-    Deserializer()
-       : _parser(*this)
-    {
-    }
-    void operations()
-    {
-        _parser.Reset();
-        _parser.CollectWord();
-        _parser.CollectWord('/');
-        _parser.CollectWord(Core::ParserType<TerminatorCarriageReturnLineFeed, Deserializer>::ParseState::UPPERCASE);
-        _parser.CollectWord('/',Core::ParserType<TerminatorCarriageReturnLineFeed, Deserializer>::ParseState::UPPERCASE);
-        _parser.CollectLine();
-        _parser.CollectLine(Core::ParserType<TerminatorCarriageReturnLineFeed, Deserializer>::ParseState::UPPERCASE);
-        _parser.FlushLine();
-        _parser.PassThrough(5);
-    }
-    ~Deserializer()
-    {
-    }
-private:
-    Parser _parser;
-};
-
-TEST(test_parser_type, simple_parser_type)
-{
-    Deserializer deserializer;
-    deserializer.operations();
-}
 TEST(test_text_parser, simple_text_parser)
 {
     TextFragment inputLine("/Service/testing/parsertest");
@@ -54,10 +22,9 @@ TEST(test_path_parser, simple_path_parser)
 {
     TextFragment inputFile("C://Service/testing/pathparsertest.txt");
     PathParser pathparser(inputFile);
-    
-    EXPECT_EQ(pathparser.Drive().Value(),'C');
-    EXPECT_STREQ(pathparser.Path().Value().Text().c_str(),_T("//Service/testing"));
-    EXPECT_STREQ(pathparser.FileName().Value().Text().c_str(),_T("pathparsertest.txt"));
-    EXPECT_STREQ(pathparser.BaseFileName().Value().Text().c_str(),_T("pathparsertest"));
-    EXPECT_STREQ(pathparser.Extension().Value().Text().c_str(),_T("txt"));;
+    pathparser.Drive();
+    pathparser.Path();
+    pathparser.FileName();
+    pathparser.BaseFileName();
+    pathparser.Extension();
 }
