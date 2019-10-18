d9bb8367fa8feae146454b2dc45daa82ce4dea7f
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-18 12:05:42 +0530

    [Tests] Updated Parser test..

--- Tests/core/test_parser.cpp
+++ Tests/core/test_parser.cpp
@@ -6,13 +6,25 @@
 using namespace WPEFramework;
 using namespace WPEFramework::Core;
 
-TEST(test_parser, simple_parser)
+TEST(test_text_parser, simple_text_parser)
 {
-    Core::TextFragment inputLine("/Service/testing/parsertest");
-    Core::TextParser textparser(inputLine);
+    TextFragment inputLine("/Service/testing/parsertest");
+    TextParser textparser(inputLine);
     textparser.Skip(2);
     textparser.Skip('S');
     //textparser.Find('t');
     textparser.SkipWhiteSpace();
     textparser.SkipLine();
+    OptionalType<TextFragment> token;
+    textparser.ReadText(token, _T("/"));
+}
+TEST(test_path_parser, simple_path_parser)
+{
+    TextFragment inputFile("C://Service/testing/pathparsertest.txt");
+    PathParser pathparser(inputFile);
+    pathparser.Drive();
+    pathparser.Path();
+    pathparser.FileName();
+    pathparser.BaseFileName();
+    pathparser.Extension();
 }
