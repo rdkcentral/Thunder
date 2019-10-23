64c922b1f1c1b43f72f6166fcf2aa4e136c22622
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-23 17:53:28 +0530

    [Tests] improved the coverage of Library test.

--- Tests/core/test_library.cpp
+++ Tests/core/test_library.cpp
@@ -11,8 +11,10 @@ TEST(Core_Library, simpleSet)
     Library libObj;
     const string file = string(BUILD_DIR) + _T("/../../Source/core/libWPEFrameworkCore.so");
     const TCHAR* function = _T("Core::Library::AddRef()");
+    const string file1 = _T("libWPEFramework.so");
     Library LibObj1(file.c_str());
     LibObj1.LoadFunction(function);
+    Library LibObjTest(file1.c_str());
     Library LibObj2(LibObj1);
     Library LibObj3;
     LibObj3 = LibObj2;
