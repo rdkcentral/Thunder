235d720d0d6ce0fadc108d8b8f3257bd274bd8bf
anjalirajan <anjali.r@tataelxsi.co.in>
2019-10-28 11:11:50 +0530

    [Tests] improved the coverage of DataElement.

--- Tests/core/test_dataelement.cpp
+++ Tests/core/test_dataelement.cpp
@@ -69,16 +69,14 @@ TEST(test_linkeddata, simple_linkeddata)
     uint8_t arr1[] ={};
     const uint64_t offset= 0;
     DataElement objt1(10,arr);
-    LinkedDataElement ob1;
-    LinkedDataElement ob2(objt1);
-    LinkedDataElement ob3(ob2);
-    ob3.Enclosed(&ob2);
-    ob3.SetBuffer(2,9,arr);
-    ob3.GetBuffer(2,9,arr1);
-    LinkedDataElement ob4;
-    ob4 = ob2;
-    EXPECT_EQ(ob4.Copy(offset,ob2), unsigned(10));
-    EXPECT_EQ(ob2.Copy(offset,ob3), unsigned(10));
+    LinkedDataElement ob1,ob2;
+    LinkedDataElement ob3(objt1,0,0);
+    LinkedDataElement opertr = ob3;
+    ob1.SetBuffer(offset,size,arr);
+    ob1.GetBuffer(offset,size,arr1);
+    uint64_t copyOb = 0;
+    EXPECT_EQ(opertr.Copy(offset,ob2), copyOb);
+    EXPECT_EQ(ob2.Copy(offset,ob3), copyOb);
 
     ob1.Enclosed();
     EXPECT_EQ(ob2.LinkedSize(), unsigned(10));
