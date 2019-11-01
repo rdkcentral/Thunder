3a86a6cdfbdf9e6043d0b17850f22219afc9ce14
anjalirajan <anjali.r@tataelxsi.co.in>
2019-11-01 12:21:49 +0530

    [Tests] improved the coverage of DataElementFile.

--- Tests/core/test_dataelementfile.cpp
+++ Tests/core/test_dataelementfile.cpp
@@ -34,14 +34,14 @@ TEST(test_datafile, simple_test)
     File fileSample(file);
     DataFile obj1(fileSample);
     DataFile object(fileName, 1, 10);
-    EXPECT_EQ(obj1.ErrorCode(),unsigned(2));
+    EXPECT_EQ(obj1.ErrorCode(),unsigned(0));
     DataFile obj2(fileName, 1, 50);
     obj1.Sync();
     obj2.MemoryMap();
 
     const string& Name = obj1.Name();
     EXPECT_EQ(Name.c_str(), fileName);
-    EXPECT_FALSE(obj2.IsValid());
+    EXPECT_TRUE(obj2.IsValid());
     obj1.Storage();
     obj1.ReloadFileInfo();
     obj1.MemoryMap();
