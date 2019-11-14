b381d7ec3d4aefc973f4f62eb2617958f1cf753f
modeveci <o.deveci@metrological.com>
2019-11-14 17:50:04 +0100

    [OCDM ADAPTER]: Remove secure buffer and use core nexus apis and also use metadata type 3 which will support non-secbuf playback

--- Source/ocdm/adapter/broadcom-svp/open_cdm_adapter.cpp
+++ Source/ocdm/adapter/broadcom-svp/open_cdm_adapter.cpp
@@ -4,17 +4,10 @@
 #include <gst/gst.h>
 #include <gst/base/gstbytereader.h>
 
-#include <b_secbuf.h>
+#include <nexus_memory.h>
 #include <gst_brcm_svp_meta.h>
 #include <assert.h>
 #include "Trace.h"
-
-struct RPCSecureBufferInformation {
-    uint32_t type;
-    size_t size;
-    void* token;
-    void* token_enc;
-};
 
 static const uint8_t nalUnit[] = {0x00, 0x00, 0x00, 0x01};
 
@@ -61,29 +54,22 @@ OpenCDMError opencdm_gstreamer_session_decrypt(struct OpenCDMSession* session, G
         uint8_t *mappedIV = nullptr;
         uint32_t mappedIVSize = 0;
         if (mappedBuffer(IV, false, &mappedIV, &mappedIVSize) == false) {
-            TRACE_L1("adapter_session_decrypt: Secbuf alloc failed!");
+            TRACE_L1("adapter_session_decrypt: Invalid IV buffer.");
             result = ERROR_INVALID_DECRYPT_BUFFER;
             goto exit;
         }
 
         uint8_t *mappedKeyID = nullptr;
         uint32_t mappedKeyIDSize = 0;
-        if (keyID != nullptr) {
-            if (mappedBuffer(keyID, false, &mappedKeyID, &mappedKeyIDSize) == false) {
-                TRACE_L1("Invalid keyID buffer.");
-                result = ERROR_INVALID_DECRYPT_BUFFER;
-                goto exit;
-            }
+        if (keyID != nullptr && mappedBuffer(keyID, false, &mappedKeyID, &mappedKeyIDSize) == false) {
+            TRACE_L1("Invalid keyID buffer.");
+            result = ERROR_INVALID_DECRYPT_BUFFER;
+            goto exit;
         }
-
-        B_Secbuf_Info secureBufferInfo;
-        void *opaqueData, *opaqueDataEnc;
-
-        uint32_t sizeOfRPCInfo = sizeof(RPCSecureBufferInformation);
-        RPCSecureBufferInformation* rpcSecureBufferInformation = reinterpret_cast<RPCSecureBufferInformation*> (g_alloca(sizeOfRPCInfo));
 
         uint32_t bufferSize = mappedDataSize;
 
+        uint32_t encryptedSampleCount = 0;
         if (mappedSubSample) {
             GstByteReader* reader = gst_byte_reader_new(mappedSubSample, mappedSubSampleSize);
             uint16_t inClear = 0;
@@ -93,6 +79,8 @@ OpenCDMError opencdm_gstreamer_session_decrypt(struct OpenCDMSession* session, G
                 gst_byte_reader_get_uint16_be(reader, &inClear);
                 gst_byte_reader_get_uint32_be(reader, &inEncrypted);
                 totalEncrypted += inEncrypted;
+                if (inEncrypted)
+                    encryptedSampleCount++;
             }
             gst_byte_reader_free(reader);
 
@@ -104,61 +92,65 @@ OpenCDMError opencdm_gstreamer_session_decrypt(struct OpenCDMSession* session, G
             bufferSize = totalEncrypted;
         }
 
-        if(B_Secbuf_Alloc(bufferSize, B_Secbuf_Type_eGeneric, &opaqueDataEnc)) {
-            TRACE_L1("adapter_session_decrypt: Secbuf alloc failed!");
-            result = ERROR_INVALID_DECRYPT_BUFFER;
-            goto exit;
-        }
-        B_Secbuf_GetBufferInfo(opaqueDataEnc, &secureBufferInfo);
-
-        rpcSecureBufferInformation->type      = secureBufferInfo.type;
-        rpcSecureBufferInformation->size      = secureBufferInfo.size;
-        rpcSecureBufferInformation->token_enc = secureBufferInfo.token;
-        rpcSecureBufferInformation->token     = NULL;
+        uint8_t* encryptedData = reinterpret_cast<uint8_t*> (::malloc(bufferSize));
+        uint8_t* encryptedDataIter = encryptedData;
 
         brcm_svp_meta_data_t* svpMeta = static_cast<brcm_svp_meta_data_t *>(g_malloc(sizeof(brcm_svp_meta_data_t)));
         memset(svpMeta, 0, sizeof(brcm_svp_meta_data_t));
-        svpMeta->sub_type = GST_META_BRCM_SVP_TYPE_2;
-        svpMeta->u.u2.chunks_cnt = subSampleCount > 0 ? subSampleCount : 1;
-        svpMeta->u.u2.chunk_info = static_cast<svp_chunk_info *>(g_malloc(svpMeta->u.u2.chunks_cnt * sizeof(svp_chunk_info)));
+        svpMeta->sub_type = GST_META_BRCM_SVP_TYPE_3;
+        svpMeta->u.u3.chunks_cnt = encryptedSampleCount > 0 ? encryptedSampleCount : 1;
+        svpMeta->u.u3.chunk_info = static_cast<svp_chunk_info *>(g_malloc(svpMeta->u.u3.chunks_cnt * sizeof(svp_chunk_info)));
 
         if (mappedSubSample) {
             GstByteReader* reader = gst_byte_reader_new(mappedSubSample, mappedSubSampleSize);
             uint16_t inClear = 0;
             uint32_t inEncrypted = 0;
-            for (uint32_t indexSec = 0, indexClr = 0, position = 0; position < subSampleCount; position++) {
+            for (uint32_t indexClr = 0, index = 0, position = 0; index < subSampleCount; index++) {
                 gst_byte_reader_get_uint16_be(reader, &inClear);
                 gst_byte_reader_get_uint32_be(reader, &inEncrypted);
 
-                B_Secbuf_ImportData(opaqueDataEnc, indexSec, mappedData + indexClr + inClear, inEncrypted, true);
-                svpMeta->u.u2.chunk_info[position].clear_size = inClear;
-                svpMeta->u.u2.chunk_info[position].encrypted_size = inEncrypted;
-                indexSec += inEncrypted;
+                // Ignore whole clean subsamples, do not process them in decryption
+                if (inEncrypted) {
+                    ::memcpy(encryptedDataIter, mappedData + indexClr + inClear, inEncrypted);
+                    svpMeta->u.u3.chunk_info[position].clear_size = inClear;
+                    svpMeta->u.u3.chunk_info[position].encrypted_size = inEncrypted;
+                    svpMeta->u.u3.chunk_info[position].offset = indexClr;
+                    encryptedDataIter += inEncrypted;
+                    position++;
+                }
                 indexClr += inClear + inEncrypted;
             }
             gst_byte_reader_free(reader);
         } else {
-            svpMeta->u.u2.chunk_info[0].clear_size = 0;
-            svpMeta->u.u2.chunk_info[0].encrypted_size = mappedDataSize;
-            B_Secbuf_ImportData(opaqueDataEnc, 0, mappedData, mappedDataSize, true);
-        }
-        // opaqueDataEnc no need as more. OCDM will acces it via its token
-        B_Secbuf_FreeDesc(opaqueDataEnc);
+            svpMeta->u.u3.chunk_info[0].clear_size = 0;
+            svpMeta->u.u3.chunk_info[0].encrypted_size = mappedDataSize;
+            svpMeta->u.u3.chunk_info[0].offset = 0;
 
-        result = opencdm_session_decrypt(session, reinterpret_cast<uint8_t*>(rpcSecureBufferInformation), sizeOfRPCInfo, mappedIV, mappedIVSize, mappedKeyID, mappedKeyIDSize, initWithLast15);
-        if(result != ERROR_NONE) {
+            ::memcpy(encryptedDataIter, mappedData , mappedDataSize);
+
+        }
+        result = opencdm_session_decrypt(session, encryptedData, bufferSize, mappedIV, mappedIVSize,
+                                         mappedKeyID, mappedKeyIDSize, initWithLast15);
+        if (result != ERROR_NONE) {
             TRACE_L1("adapter_session_decrypt: opencdm_session_decrypt failed!");
+            ::free(encryptedData);
             goto exit;
         }
 
-        // OCDM allocate opaqueData for secure decrypted buffer and will be freed in gstreamer
-        if(B_Secbuf_AllocWithToken(bufferSize, B_Secbuf_Type_eSecure,  rpcSecureBufferInformation->token, &opaqueData)) {
-            TRACE_L1("adapter_session_decrypt: Secbuf Alloc failed!");
-            result = ERROR_INVALID_DECRYPT_BUFFER;
-            goto exit;
+        uint32_t *tokenHandle = reinterpret_cast<uint32_t *>(encryptedData);
+        NEXUS_MemoryBlockHandle block = nullptr;
+        block = NEXUS_MemoryBlock_Clone (reinterpret_cast<NEXUS_MemoryBlockTokenHandle>(*tokenHandle));
+        if (!block) {
+            TRACE_L1("Memory token alloc error");
+        }
+        ::free(encryptedData);
+        void *opaqueData;
+        NEXUS_MemoryBlock_Lock(block, &opaqueData);
+        if (!opaqueData) {
+            TRACE_L1("Memory token alloc error");
         }
 
-        svpMeta->u.u2.secbuf_ptr = reinterpret_cast<uintptr_t>(opaqueData);
+        svpMeta->u.u3.secbuf_ptr = reinterpret_cast<uintptr_t>(opaqueData);
         gst_buffer_add_brcm_svp_meta(buffer, svpMeta);
     }
 exit:
