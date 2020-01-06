6762717831674159bb5ca4292c9581a2f4ff33d2
anjalirajan <anjali.r@tataelxsi.co.in>
2020-01-06 12:20:41 +0530

    [Tests] Improved coverage of ISO639 test.

--- Tests/core/test_iso639.cpp
+++ Tests/core/test_iso639.cpp
@@ -11,6 +11,9 @@ TEST(test_ISO639, simple_ISO639)
     Language();
     Language lang("eng");
     Language(12);
+    Language lang1(" ");
+    Language lang2("en");
+    Language lang3("aaaaaa");
      
     EXPECT_TRUE(lang.IsValid());
     EXPECT_STREQ(lang.LetterCode3(),"eng");
