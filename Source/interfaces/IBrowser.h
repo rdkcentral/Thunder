9629e13e398f1bd5c869f9c3c01bce0021604eee
sebaszm <s.szczepaniak@metrological.com>
2020-01-10 01:30:21 +0100

    [interfaces] Introduce IWebBrowser

--- Source/interfaces/IBrowser.h
+++ Source/interfaces/IBrowser.h
@@ -8,6 +8,7 @@ namespace Exchange {
 
     // This interface gives direct access to a Browser to change
     // Browser specific properties like displayed URL.
+
     struct IBrowser : virtual public Core::IUnknown {
         enum { ID = ID_BROWSER };
 
@@ -23,16 +24,6 @@ namespace Exchange {
             virtual void Closure() = 0;
         };
 
-        struct IMetadata : virtual public Core::IUnknown {
-            enum { ID = ID_BROWSER_METADATA };
-
-            virtual ~IMetadata() {}
-
-            virtual string LocalCache() const = 0;
-            virtual string CookieStore() const = 0;
-            virtual void GarbageCollect() = 0;
-        };
-
         virtual ~IBrowser() {}
 
         virtual void Register(IBrowser::INotification* sink) = 0;
@@ -41,10 +32,42 @@ namespace Exchange {
         // Change the currenly displayed URL by the browser.
         virtual void SetURL(const string& URL) = 0;
         virtual string GetURL() const = 0;
+
         virtual uint32_t GetFPS() const = 0;
 
         virtual void Hide(const bool hidden) = 0;
     };
+
+    /* @json */
+    struct IWebBrowser : virtual public Core::IUnknown {
+        enum { ID = ID_WEB_BROWSER };
+
+        /* @event */
+        using INotification = IBrowser::INotification;
+
+        virtual ~IWebBrowser() { }
+
+        virtual void Register(INotification* sink) = 0;
+        virtual void Unregister(INotification* sink) = 0;
+
+        /* @property */
+        /* @brief Page loaded in the browser */
+        /* @param url Loaded URL */
+        virtual uint32_t URL(string& url /* @out */) const = 0;
+        virtual uint32_t URL(const string& url) = 0;
+
+        /* @property */
+        /* @brief Browser window visibility state */
+        /* @param visible Visiblity state */
+        virtual uint32_t Visible(bool visible /* @out */) const = 0;
+        virtual uint32_t Visible(const bool visible) = 0;
+
+        /* @property*/
+        /* @brief Current framerate the browser is rendering at */
+        /* @param fps Current FPS */
+        virtual uint32_t FPS(uint8_t& fps /* @out */) const = 0;
+    };
+
 }
 }
 
