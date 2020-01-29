befb8cd96edac5520183978c1d268c7d8f1bc09d
sebaszm <s.szczepaniak@metrological.com>
2020-01-29 01:23:04 +0100

    [Interfaces] update VolumeControl with doc tags

--- Source/interfaces/IVolumeControl.h
+++ Source/interfaces/IVolumeControl.h
@@ -16,9 +16,11 @@ namespace Exchange {
             virtual ~INotification() {}
 
             // @brief Signals volume change
+            // @param volume New bolume level in percent (e.g. 100)
             virtual void Volume(const uint8_t volume) = 0;
 
             // @brief Signals mute state change
+            // @param muted New mute state (true: muted, false: un-muted)
             virtual void Muted(const bool muted) = 0;
         };
 
@@ -29,11 +31,15 @@ namespace Exchange {
 
         // @property
         // @brief Audio mute state
+        // @param muted Mute state (true: muted, false: un-muted)
+        // @retval ERROR_GENERAL Failed to set/retrieve muting state
         virtual uint32_t Muted(const bool muted) = 0;
         virtual uint32_t Muted(bool& muted /* @out */) const = 0;
 
         // @property
-        // @brief Audio volume level (in percent)
+        // @brief Audio volume level
+        // @param volume Volume level in percent (e.g. 100)
+        // @retval ERROR_GENERAL Failed to set/retrieve audio volume
         virtual uint32_t Volume(const uint8_t volume) = 0;
         virtual uint32_t Volume(uint8_t& volume /* @out */) const = 0;
     };
