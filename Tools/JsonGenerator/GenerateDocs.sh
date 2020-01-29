10e7098e90228cee0d2bb7bf2f6a1afb0f09ea63
sebaszm <s.szczepaniak@metrological.com>
2020-01-29 19:38:53 +0100

    [Tools] Simpify documentation generation

--- Tools/JsonGenerator/GenerateDocs.sh
+++ Tools/JsonGenerator/GenerateDocs.sh
@@ -3,25 +3,26 @@
 #
 # Generates Plugin Markdown documentation.
 #
-# Example:
-#   ./GenerateDocs ../../../WPEFrameworkPlugins/Streamer/StreamerPlugin.json
-#   ./GenerateDocs ../../../WPEFrameworkPlugins/Streamer/StreamerPlugin.json ../../../WPEFrameworkPlugins/TimeSync/TimeSyncPlugin.json
-#   ./GenerateDocs -d ../../../WPEFrameworkPlugins
+# Typical usage:
+#   ./GenerateDocs ../../../ThunderNanoServices/
+#   ./GenerateDocs ../../../WPEFrameworkPlugins/BluetoothControl
+#   ./GenerateDocs ../../../WPEFrameworkPlugins/BluetoothControl/BluetoothControlPlugin.json
 #
 
 command -v ./JsonGenerator.py >/dev/null 2>&1 || { echo >&2 "JsonGenerator.py is not available. Aborting."; exit 1; }
 
-if [ "$1" = "-d" ]; then
-   files=`find $2 -name "*Plugin.json"`
+if [ -d ]; then
+   files=`find $1 -name "*Plugin.json"`
 elif [ $# -gt 0 ]; then
    files=$@
 else
    echo >&2 "usage: $0 [-d <directory>|<json> ...]"
+   echo Note: Directory scan is recursive
    exit 0
 fi
 
 echo "Generating Plugin markdown documentation..."
 
-./JsonGenerator.py --docs -i ../../Source/interfaces/json -j ../../Source/interfaces $files -o doc
+./JsonGenerator.py --docs -i ../../Source/interfaces/json -j ../../Source/interfaces -o doc $files
 
-echo "Done."
+echo "Complete."
