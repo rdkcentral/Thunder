650e302d9fcf6be34478b5eb2f51eab0487d8cb2
anjalirajan <anjali.r@tataelxsi.co.in>
2020-03-23 08:39:38 +0530

    [Tests] Updated based on review comments.

--- Tests/core/test_websocketjson.cpp
+++ Tests/core/test_websocketjson.cpp
@@ -3,159 +3,205 @@
 #include <gtest/gtest.h>
 #include <core/core.h>
 #include <websocket/websocket.h>
+#include <condition_variable>
+#include <mutex>
 
 namespace WPEFramework {
 namespace Tests {
 
-namespace JsonWebSocketTest {
-    bool g_done = false;
-    const TCHAR* g_connector = "/tmp/wpewebsocketjson0";
-} // JsonWebSocketTest
+    const TCHAR* g_webJsonConnector = "/tmp/wpewebsocketjson0";
 
-	class Message : public Core::JSON::Container {
-	private:
-		Message(const Message&) = delete;
-		Message& operator= (const Message&) = delete;
+    class Message : public Core::JSON::Container {
+    public:
+        Message(const Message&) = delete;
+	    Message& operator= (const Message&) = delete;
 
-	public:
-		Message() :
-			Core::JSON::Container(),
-			EventType(),
-			Event() {
-			Add(_T("eventType"), &EventType);
-			Add(_T("event"), &Event);
-		}
-		~Message() {
-		}
+	    Message()
+            : Core::JSON::Container()
+            , EventType()
+            , Event()
+        {
+            Add(_T("eventType"), &EventType);
+            Add(_T("event"), &Event);
+        }
 
-	public:
-		Core::JSON::String EventType;
-		Core::JSON::String Event;
-	};
+        ~Message()
+        {
+        }
 
-	class Factory : public Core::ProxyPoolType<Message> {
-	private:
-		Factory() = delete;
-		Factory(const Factory&) = delete;
-		Factory& operator= (const Factory&) = delete;
+    public:
+	    Core::JSON::String EventType;
+	    Core::JSON::String Event;
+    };
 
-	public:
-		Factory(const uint32_t number) : Core::ProxyPoolType<Message>(number) {
-		}
-		virtual ~Factory() {
-		}
+    class Factory : public Core::ProxyPoolType<Message> {
+    public:
+	    Factory() = delete;
+	    Factory(const Factory&) = delete;
+	    Factory& operator= (const Factory&) = delete;
 
-	public:
-		Core::ProxyType<Core::JSON::IElement> Element(const string&) {
-			return (Core::proxy_cast<Core::JSON::IElement>(Core::ProxyPoolType<Message>::Element()));
-		}
-	};
+	    Factory(const uint32_t number) : Core::ProxyPoolType<Message>(number)
+        {
+	    }
+
+	    virtual ~Factory()
+        {
+	    }
+
+    public:
+	    Core::ProxyType<Core::JSON::IElement> Element(const string&)
+        {
+		    return (Core::proxy_cast<Core::JSON::IElement>(Core::ProxyPoolType<Message>::Element()));
+	    }
+    };
 
     template<typename INTERFACE>
-	class JsonSocketServer : public Core::StreamJSONType< Web::WebSocketServerType<Core::SocketStream>, Factory&, INTERFACE> {
-	private:
-		typedef Core::StreamJSONType< Web::WebSocketServerType<Core::SocketStream>, Factory&, INTERFACE> BaseClass;
+    class JsonSocketServer : public Core::StreamJSONType< Web::WebSocketServerType<Core::SocketStream>, Factory&, INTERFACE> {
+    private:
+	    typedef Core::StreamJSONType< Web::WebSocketServerType<Core::SocketStream>, Factory&, INTERFACE> BaseClass;
 
-	private:
-		JsonSocketServer(const JsonSocketServer&) = delete;
-		JsonSocketServer& operator=(const JsonSocketServer&) = delete;
+    public:
+        JsonSocketServer() = delete;
+        JsonSocketServer(const JsonSocketServer&) = delete;
+	    JsonSocketServer& operator=(const JsonSocketServer&) = delete;
 
-	public:
-		JsonSocketServer(const SOCKET& socket, const Core::NodeId& remoteNode, Core::SocketServerType<JsonSocketServer<INTERFACE>>*)
-			: BaseClass(2, _objectFactory, false, false, false, socket, remoteNode, 512, 512)
-			, _objectFactory(1) {
-		}
-		virtual ~JsonSocketServer() {
-		}
+        JsonSocketServer(const SOCKET& socket, const Core::NodeId& remoteNode, Core::SocketServerType<JsonSocketServer<INTERFACE>>*)
+            : BaseClass(2, _objectFactory, false, false, false, socket, remoteNode, 512, 512)
+		    , _objectFactory(1)
+        {
+	    }
 
-	public:
-		virtual bool IsIdle() const {
+        virtual ~JsonSocketServer()
+        {
+        }
+
+    public:
+	    virtual bool IsIdle() const
+        {
             return (true);
         }
-		virtual void StateChange() {
-			if (this->IsOpen())
-                JsonWebSocketTest::g_done = true;
-		}
 
-		bool IsAttached() const {
-			return (this->IsOpen());
-		}
-		virtual void Received(Core::ProxyType<Core::JSON::IElement>& jsonObject) {
+	    virtual void StateChange()
+        {
+		    if (this->IsOpen()) {
+                std::unique_lock<std::mutex> lk(_mutex);
+                _done = true;
+                _cv.notify_one();
+            }
+        }
+
+        bool IsAttached() const
+        {
+            return (this->IsOpen());
+        }
+
+        virtual void Received(Core::ProxyType<Core::JSON::IElement>& jsonObject)
+        {
             this->Submit(jsonObject);
-		}
-		virtual void Send(Core::ProxyType<Core::JSON::IElement>& jsonObject) {
-		}
-	private:
-		Factory _objectFactory;
-	};
+        }
+
+        virtual void Send(Core::ProxyType<Core::JSON::IElement>& jsonObject)
+        {
+	    }
+
+        static bool GetState()
+        {
+            return _done;
+        }
+
+    private:
+	    Factory _objectFactory;
+        static bool _done;
+
+    public:
+        static std::mutex _mutex;
+        static std::condition_variable _cv;
+    };
 
     template<typename INTERFACE>
-	class JsonSocketClient : public Core::StreamJSONType<Web::WebSocketClientType<Core::SocketStream>, Factory&, INTERFACE> {
-	private:
+    std::mutex JsonSocketServer<INTERFACE>::_mutex;
+    template<typename INTERFACE>
+    std::condition_variable JsonSocketServer<INTERFACE>::_cv;
+    template<typename INTERFACE>
+    bool JsonSocketServer<INTERFACE>::_done = false;
+
+    template<typename INTERFACE>
+    class JsonSocketClient : public Core::StreamJSONType<Web::WebSocketClientType<Core::SocketStream>, Factory&, INTERFACE> {
+    private:
 		typedef Core::StreamJSONType<Web::WebSocketClientType<Core::SocketStream>, Factory&, INTERFACE> BaseClass;
 
-	private:
-		JsonSocketClient(const JsonSocketClient&) = delete;
-		JsonSocketClient& operator=(const JsonSocketClient&) = delete;
+    public:
+        JsonSocketClient() = delete;
+        JsonSocketClient(const JsonSocketClient&) = delete;
+        JsonSocketClient& operator=(const JsonSocketClient&) = delete;
 
-	public:
-		JsonSocketClient(const Core::NodeId& remoteNode)
-			: BaseClass(5, _objectFactory, _T(""), _T(""), _T(""), _T(""), false, true, false, remoteNode.AnyInterface(), remoteNode, 256, 256)
-			, _objectFactory(2)
+        JsonSocketClient(const Core::NodeId& remoteNode)
+		    : BaseClass(5, _objectFactory, _T(""), _T(""), _T(""), _T(""), false, true, false, remoteNode.AnyInterface(), remoteNode, 256, 256)
+            , _objectFactory(2)
             , _dataPending(false, false)
-		{
-		}
-		virtual ~JsonSocketClient()
-		{
-		}
+        {
+        }
 
-	public:
-		virtual void Received(Core::ProxyType<Core::JSON::IElement>& jsonObject)
-		{
-			string textElement;
-			jsonObject->ToString(textElement);
+        virtual ~JsonSocketClient()
+        {
+        }
+
+    public:
+	    virtual void Received(Core::ProxyType<Core::JSON::IElement>& jsonObject)
+	    {
+		    string textElement;
+		    jsonObject->ToString(textElement);
             _dataReceived = textElement;
             _dataPending.Unlock();
-            
-		}
-		virtual void Send(Core::ProxyType<Core::JSON::IElement>& jsonObject)
-		{
-		}
-		virtual void StateChange()
-		{
-		}
-		virtual bool IsIdle() const
-		{
-			return (true);
-		}
+        }
+
+        virtual void Send(Core::ProxyType<Core::JSON::IElement>& jsonObject)
+	    {
+        }
+
+        virtual void StateChange()
+	    {
+        }
+
+        virtual bool IsIdle() const
+	    {
+		    return (true);
+        }
+
 
         Core::ProxyType<Core::JSON::IElement> Element()
         {
             return _objectFactory.Element("");
-        } 
+        }
+
         int Wait() const
         {
             return _dataPending.Lock();
         }
+
         void Retrieve(string& text)
         {
             text = _dataReceived;
             _dataReceived.clear();
         }
-	private:
-		Factory _objectFactory;
+
+        private:
+        Factory _objectFactory;
         string _dataReceived;
         mutable Core::Event _dataPending;
-	};
+    };
 
     TEST(WebSocket, Json)
     {
         IPTestAdministrator::OtherSideMain otherSide = [](IPTestAdministrator & testAdmin) {
-            Core::SocketServerType<JsonSocketServer<Core::JSON::IElement>> jsonWebSocketServer(Core::NodeId(JsonWebSocketTest::g_connector));
+            Core::SocketServerType<JsonSocketServer<Core::JSON::IElement>> jsonWebSocketServer(Core::NodeId(Tests::g_webJsonConnector));
             jsonWebSocketServer.Open(Core::infinite);
             testAdmin.Sync("setup server");
-            sleep(2);
-            while(!JsonWebSocketTest::g_done);
+
+            std::unique_lock<std::mutex> lk(JsonSocketServer<Core::JSON::IElement>::_mutex);
+            while(!JsonSocketServer<Core::JSON::IElement>::GetState()) {
+                JsonSocketServer<Core::JSON::IElement>::_cv.wait(lk);
+            }
             testAdmin.Sync("server open");
             testAdmin.Sync("client done");
         };
@@ -169,7 +215,7 @@ namespace JsonWebSocketTest {
             std::string sendString;
             sendObject->ToString(sendString);
 
-            JsonSocketClient<Core::JSON::IElement> jsonWebSocketClient(Core::NodeId(JsonWebSocketTest::g_connector));
+            JsonSocketClient<Core::JSON::IElement> jsonWebSocketClient(Core::NodeId(Tests::g_webJsonConnector));
             jsonWebSocketClient.Open(Core::infinite);
             testAdmin.Sync("server open");
             jsonWebSocketClient.Submit(Core::proxy_cast<Core::JSON::IElement>(sendObject));
