789e3667d4af439a0039d42eb0d1040cbd17d337
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2020-05-29 14:23:13 +0200

    DisplayInfo: Put Tracing behind debug flag

--- Source/displayinfo/DisplayInfo.cpp
+++ Source/displayinfo/DisplayInfo.cpp
@@ -38,11 +38,15 @@ static string GetEndPoint()
                              : value);
 }
 
+#ifdef __DEBUG__
 #define Trace(fmt, ...)                                                                                                                     \
     do {                                                                                                                                    \
         fprintf(stdout, "\033[1;32m[%s:%d](%s){%p}<%d>:" fmt "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, this, getpid(), ##__VA_ARGS__); \
         fflush(stdout);                                                                                                                     \
     } while (0)
+#else
+#define Trace(fmt, ...)   
+#endif
 
 namespace WPEFramework {
 
