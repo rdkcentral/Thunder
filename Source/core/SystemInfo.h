5ba5a508bf66f982f639ffffeb1e6be6898c0cf9
Pierre Wielders <pierre@wielders.net>
2020-07-15 15:24:35 +0200

    [EXTERNAL] Hunting down the Core missing external.

--- Source/core/SystemInfo.h
+++ Source/core/SystemInfo.h
@@ -335,17 +335,17 @@ namespace Core {
 
 #define MODULE_BUILDREF MODULE_NAME##Version
 
-#define MODULE_NAME_DECLARATION(buildref)                                                                     \
-    extern "C" {                                                                                              \
-    namespace WPEFramework {                                                                                  \
-        namespace Core {                                                                                      \
-            namespace System {                                                                                \
-                const char* MODULE_NAME = SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(MODULE_NAME);              \
-                const char* ModuleName() { return (MODULE_NAME); }                                            \
-                const char* ModuleBuildRef() { return (SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(buildref)); } \
-            }                                                                                                 \
-        }                                                                                                     \
-    }                                                                                                         \
+#define MODULE_NAME_DECLARATION(buildref)                                                                              \
+    extern "C" {                                                                                                       \
+    namespace WPEFramework {                                                                                           \
+        namespace Core {                                                                                               \
+            namespace System {                                                                                         \
+                EXTERNAL const char* MODULE_NAME = SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(MODULE_NAME);              \
+                EXTERNAL const char* ModuleName() { return (MODULE_NAME); }                                            \
+                EXTERNAL const char* ModuleBuildRef() { return (SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(buildref)); } \
+            }                                                                                                          \
+        }                                                                                                              \
+    }                                                                                                                  \
     } // extern "C" Core::System
 
 #endif // __SYSTEMINFO_H
