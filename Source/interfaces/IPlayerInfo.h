1398f24ed3cfa9e89f795e64f153ba5725656f0b
Pierre Wielders <pierre@wielders.net>
2020-08-29 10:14:11 +0200

    [IDisplayInfo] Update for Thunder using the newly proposed IDisplay/IPlayerInfo/IDolby interfaces.

--- Source/interfaces/IPlayerInfo.h
+++ Source/interfaces/IPlayerInfo.h
@@ -23,60 +23,67 @@
 
 namespace WPEFramework {
 namespace Exchange {
-
+    /* @json */
     struct EXTERNAL IPlayerProperties : virtual public Core::IUnknown {
         enum { ID = ID_PLAYER_PROPERTIES };
         virtual ~IPlayerProperties() {}
 
-        struct EXTERNAL IAudioIterator : virtual public Core::IUnknown {
-            enum { ID = ID_PLAYER_PROPERTIES_AUDIO };
-            virtual ~IAudioIterator() {}
-
-            enum AudioCodec : uint8_t {
-                UNDEFINED = 0,
-                AUDIO_AAC,
-                AUDIO_AC3,
-                AUDIO_AC3_PLUS,
-                AUDIO_DTS,
-                AUDIO_MPEG1,
-                AUDIO_MPEG2,
-                AUDIO_MPEG3,
-                AUDIO_MPEG4,
-                AUDIO_OPUS,
-                AUDIO_VORBIS_OGG,
-                AUDIO_WAV
-            };
-
-            virtual bool IsValid() const = 0;
-            virtual bool Next() = 0;
-            virtual void Reset() = 0;
-            virtual AudioCodec Codec() const = 0;
+         enum AudioCodec : uint8_t {
+            AUDIO_UNDEFINED,
+            AUDIO_AAC,
+            AUDIO_AC3,
+            AUDIO_AC3_PLUS,
+            AUDIO_DTS,
+            AUDIO_MPEG1,
+            AUDIO_MPEG2,
+            AUDIO_MPEG3,
+            AUDIO_MPEG4,
+            AUDIO_OPUS,
+            AUDIO_VORBIS_OGG,
+            AUDIO_WAV
         };
 
-        struct EXTERNAL IVideoIterator : virtual public Core::IUnknown {
-            enum { ID = ID_PLAYER_PROPERTIES_VIDEO };
-            virtual ~IVideoIterator() {}
+        typedef RPC::IIteratorType<AudioCodec, ID_PLAYER_PROPERTIES_AUDIO> IAudioCodedIterator;
 
-            enum VideoCodec : uint8_t {
-                UNDEFINED = 0,
-                VIDEO_H263,
-                VIDEO_H264,
-                VIDEO_H265,
-                VIDEO_H265_10,
-                VIDEO_MPEG,
-                VIDEO_VP8,
-                VIDEO_VP9,
-                VIDEO_VP10
-            };
-
-            virtual bool IsValid() const = 0;
-            virtual bool Next() = 0;
-            virtual void Reset() = 0;
-            virtual VideoCodec Codec() const = 0;
+        enum VideoCodec : uint8_t {
+            VIDEO_UNDEFINED,
+            VIDEO_H263,
+            VIDEO_H264,
+            VIDEO_H265,
+            VIDEO_H265_10,
+            VIDEO_MPEG,
+            VIDEO_VP8,
+            VIDEO_VP9,
+            VIDEO_VP10
         };
 
-        virtual IAudioIterator* AudioCodec() const = 0;
-        virtual IVideoIterator* VideoCodec() const = 0;
+        typedef RPC::IIteratorType<VideoCodec, ID_PLAYER_PROPERTIES_VIDEO> IVideoCodecIterator;
+
+        virtual uint32_t AudioCodec(IAudioCodedIterator& codec /* @out */) const;
+        virtual uint32_t* VideoCodec(IVideoCodecIterator& codec /* @out */) const;
+
+        enum PlaybackResolution : uint8_t {
+            RESOLUTION_UNKNOWN,
+            RESOLUTION_480I,
+            RESOLUTION_480P,
+            RESOLUTION_576I,
+            RESOLUTION_576P,
+            RESOLUTION_720P,
+            RESOLUTION_1080I,
+            RESOLUTION_1080P,
+            RESOLUTION_2160P30,
+            RESOLUTION_2160P60
+        };
+
+        // @property
+        // @brief Current Video playback resolution
+        // @param res: resolution
+        virtual uint32_t Resolution(PlaybackResolution& res /* @out */) const = 0;
+
+        // @property
+        // @brief Checks Loudness Equivalence in platform
+        // @param ae: enabled/disabled
+        virtual uint32_t IsAudioEquivalenceEnabled(bool& ae /* @out */) const = 0;
     };
 }
 }
