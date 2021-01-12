74c92ea0992165a61856ee0429ef7dcc7077dc31
Lukasz Iwan <l.iwan@metrological.com>
2021-01-12 07:55:43 -0800

    Removed multiple *WaitTime, using the longest one: DefaultWaitTime

--- Source/websocket/JSONRPCLink.h
+++ Source/websocket/JSONRPCLink.h
@@ -1235,7 +1235,6 @@ namespace JSONRPC {
     private:
         class Connection : public LinkType<INTERFACE> {
         private:
-            static constexpr uint32_t ConnectionWaitTime = 3000;
             using Base = LinkType<INTERFACE>;
         public:
             static constexpr uint32_t DefaultWaitTime = Base::DefaultWaitTime;
@@ -1371,7 +1370,7 @@ namespace JSONRPC {
             {
                 if (result == nullptr) {
                     string method = string("status@") + Base::Callsign();
-                    _monitor.template Dispatch<void>(ConnectionWaitTime, method, &Connection::monitor_response, this);
+                    _monitor.template Dispatch<void>(DefaultWaitTime, method, &Connection::monitor_response, this);
                 }
             }
             void next_event(const Core::JSON::String& parameters, const Core::JSONRPC::Error* result)
@@ -1380,7 +1379,7 @@ namespace JSONRPC {
                 if (_events.empty() == false) {
                     const string parameters("{ \"event\": \"" + _events.front() + "\", \"id\": \"" + Base::Namespace() + "\"}");
                     _events.pop_front();
-                    LinkType<INTERFACE>::Dispatch(ConnectionWaitTime, _T("register"), parameters, &Connection::next_event, this);
+                    LinkType<INTERFACE>::Dispatch(DefaultWaitTime, _T("register"), parameters, &Connection::next_event, this);
                 }
                 else {
                     SetState(JSONRPC::JSONPluginState::ACTIVATED);
@@ -1392,7 +1391,7 @@ namespace JSONRPC {
                 // Time to open up the monitor
                 const string parameters("{ \"event\": \"statechange\", \"id\": \"" + _monitor.Namespace() + "\"}");
 
-                _monitor.template Dispatch<string>(ConnectionWaitTime, "register", parameters, &Connection::monitor_on, this);
+                _monitor.template Dispatch<string>(DefaultWaitTime, "register", parameters, &Connection::monitor_on, this);
             }
 
         private:
