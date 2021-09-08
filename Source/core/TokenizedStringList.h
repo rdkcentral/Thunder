b0611d297eec43e354e5b45e867f688eeeedd209
Lukasz Iwan <l.iwan@metrological.com>
2021-09-08 14:58:54 +0200

    Create class able to parse and tokenize string based on separator

---
+++ Source/core/TokenizedStringList.h
@@ -0,0 +1,108 @@
+#pragma once
+#include "Module.h"
+
+namespace WPEFramework {
+namespace Core {
+
+template <char SEPARATOR, bool REMOVE_WHITESPACES>
+class TokenizedStringList {
+public:
+    TokenizedStringList() = default;
+    ~TokenizedStringList() = default;
+    TokenizedStringList(const TokenizedStringList&) = default;
+    TokenizedStringList& operator=(const TokenizedStringList&) = default;
+
+    explicit TokenizedStringList(const string& buffer)
+    {
+        Tokenize(buffer);
+    }
+
+    void Tokenize(string buffer)
+    {
+        if (!buffer.empty()) {
+            //remove spaces and tokenize by comma
+            if (REMOVE_WHITESPACES) {
+                buffer.erase(std::remove_if(buffer.begin(), buffer.end(), ::isspace), buffer.end());
+            }
+            std::istringstream iss(buffer);
+
+            string substring;
+            while (iss.good()) {
+                getline(iss, substring, SEPARATOR);
+                _parts.push_back(substring);
+            }
+        }
+    }
+
+    void Clear()
+    {
+        _parts.clear();
+    }
+
+    const string& First() const
+    {
+        return _parts.front();
+    }
+
+    string All() const
+    {
+        string result;
+
+        if (!_parts.empty()) {
+
+            for (const auto& part : _parts) {
+                result += part;
+                result += ',';
+            }
+
+            result.erase(result.rfind(','));
+        }
+
+        return result;
+    }
+
+    bool Empty() const
+    {
+        return _parts.empty();
+    }
+
+    std::size_t Size() const
+    {
+        return _parts.size();
+    }
+
+    std::vector<string>::iterator begin()
+    {
+        return _parts.begin();
+    }
+
+    std::vector<string>::iterator end()
+    {
+        return _parts.end();
+    }
+
+    std::vector<string>::const_iterator begin() const
+    {
+        return _parts.cbegin();
+    }
+
+    std::vector<string>::const_iterator end() const
+    {
+        return _parts.cend();
+    }
+
+    std::string& operator[](int index)
+    {
+        return _parts[index];
+    }
+
+    const std::string& operator[](int index) const
+    {
+        return _parts[index];
+    }
+
+private:
+    std::vector<string> _parts;
+};
+}
+}
