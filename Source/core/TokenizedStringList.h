a3397bd01c1c9a96fe2679f1be07b0cec965a844
Lukasz Iwan <l.iwan@metrological.com>
2021-09-09 09:36:02 +0200

    Bugfixes, applied formatting

--- Source/core/TokenizedStringList.h
+++ Source/core/TokenizedStringList.h
@@ -4,105 +4,108 @@
 namespace WPEFramework {
 namespace Core {
 
-template <char SEPARATOR, bool REMOVE_WHITESPACES>
-class TokenizedStringList {
-public:
-    TokenizedStringList() = default;
-    ~TokenizedStringList() = default;
-    TokenizedStringList(const TokenizedStringList&) = default;
-    TokenizedStringList& operator=(const TokenizedStringList&) = default;
+    template <char SEPARATOR, bool REMOVE_WHITESPACES>
+    class TokenizedStringList {
+    public:
+        TokenizedStringList() = default;
+        ~TokenizedStringList() = default;
+        TokenizedStringList(const TokenizedStringList&) = default;
+        TokenizedStringList& operator=(const TokenizedStringList&) = default;
 
-    explicit TokenizedStringList(const string& buffer)
-    {
-        Tokenize(buffer);
-    }
-
-    void Tokenize(string buffer)
-    {
-        if (!buffer.empty()) {
-            //remove spaces and tokenize by comma
-            if (REMOVE_WHITESPACES) {
-                buffer.erase(std::remove_if(buffer.begin(), buffer.end(), ::isspace), buffer.end());
-            }
-            std::istringstream iss(buffer);
-
-            string substring;
-            while (iss.good()) {
-                getline(iss, substring, SEPARATOR);
-                _parts.push_back(substring);
-            }
-        }
-    }
-
-    void Clear()
-    {
-        _parts.clear();
-    }
-
-    const string& First() const
-    {
-        return _parts.front();
-    }
-
-    string All() const
-    {
-        string result;
-
-        if (!_parts.empty()) {
-
-            for (const auto& part : _parts) {
-                result += part;
-                result += ',';
-            }
-
-            result.erase(result.rfind(','));
+        explicit TokenizedStringList(const string& buffer)
+        {
+            Tokenize(buffer);
         }
 
-        return result;
-    }
+        void Tokenize(const string& buffer)
+        {
 
-    bool Empty() const
-    {
-        return _parts.empty();
-    }
+            if (!buffer.empty()) {
+                //remove spaces and tokenize by comma
+                if (REMOVE_WHITESPACES) {
+                    string result = buffer;
+                    result.erase(std::remove_if(result.begin(), result.end(), ::isspace), result.end());
+                    std::istringstream iss(result);
+                }
+                std::istringstream iss(buffer);
 
-    std::size_t Size() const
-    {
-        return _parts.size();
-    }
+                string substring;
+                while (iss.good()) {
+                    getline(iss, substring, SEPARATOR);
+                    _parts.push_back(substring);
+                }
+            }
+        }
 
-    std::vector<string>::iterator begin()
-    {
-        return _parts.begin();
-    }
+        void Clear()
+        {
+            _parts.clear();
+        }
 
-    std::vector<string>::iterator end()
-    {
-        return _parts.end();
-    }
+        const string& First() const
+        {
+            return _parts.front();
+        }
 
-    std::vector<string>::const_iterator begin() const
-    {
-        return _parts.cbegin();
-    }
+        string All() const
+        {
+            string result;
 
-    std::vector<string>::const_iterator end() const
-    {
-        return _parts.cend();
-    }
+            if (!_parts.empty()) {
 
-    std::string& operator[](int index)
-    {
-        return _parts[index];
-    }
+                for (const auto& part : _parts) {
+                    result += part;
+                    result += SEPARATOR;
+                }
 
-    const std::string& operator[](int index) const
-    {
-        return _parts[index];
-    }
+                result.erase(result.rfind(','));
+            }
 
-private:
-    std::vector<string> _parts;
-};
+            return result;
+        }
+
+        bool Empty() const
+        {
+            return _parts.empty();
+        }
+
+        std::size_t Size() const
+        {
+            return _parts.size();
+        }
+
+        std::vector<string>::iterator begin()
+        {
+            return _parts.begin();
+        }
+
+        std::vector<string>::iterator end()
+        {
+            return _parts.end();
+        }
+
+        std::vector<string>::const_iterator begin() const
+        {
+            return _parts.cbegin();
+        }
+
+        std::vector<string>::const_iterator end() const
+        {
+            return _parts.cend();
+        }
+
+        std::string& operator[](int index)
+        {
+            return _parts[index];
+        }
+
+        const std::string& operator[](int index) const
+        {
+            return _parts[index];
+        }
+
+    private:
+        std::vector<string> _parts;
+    };
 }
 }
