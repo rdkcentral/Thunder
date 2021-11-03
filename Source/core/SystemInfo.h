cd458ada3f38b8b63235eadd0f7e2f31fda0e04b
Adam Stolcenburg <astolcenburg.contractor@libertyglobal.com>
2021-11-03 15:38:48 +0100

    ONEM-23136 Export symbols related to module name

--- Source/core/SystemInfo.h
+++ Source/core/SystemInfo.h
@@ -366,9 +366,9 @@ namespace Core {
     namespace WPEFramework {                                                                                           \
         namespace Core {                                                                                               \
             namespace System {                                                                                         \
-                const char* MODULE_NAME = SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(MODULE_NAME);              \
-                const char* ModuleName() { return (MODULE_NAME); }                                            \
-                const char* ModuleBuildRef() { return (SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(buildref)); } \
+                EXTERNAL const char* MODULE_NAME = SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(MODULE_NAME);              \
+                EXTERNAL const char* ModuleName() { return (MODULE_NAME); }                                            \
+                EXTERNAL const char* ModuleBuildRef() { return (SOLUTIONS_GENERICS_SYSTEM_PREPROCESSOR_2(buildref)); } \
             }                                                                                                          \
         }                                                                                                              \
     }                                                                                                                  \
