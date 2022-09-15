08c8f8e8006082b61884ed687ff2fa5e75cdc9c7
zrash <zexpen@gmail.com>
2022-09-15 23:23:19 +0530

    DELIA-57764,DELIA-57663: fix LD_LIBRARY_PATH being overwritten (#1056)
    Reason for change: Lauch of YT fails due to wrong LD_LIBRARY_PATH
    
    Test Procedure: Build and verify.
    Risks: low
    Signed-off-by:Zameerun Rasheed M S <zmamoo711@cable.comcast.com>
    
    Co-authored-by: Zameerun Rasheed M S <ZameerunRasheed_MamoonSyed@comcast.com>

--- Source/WPEProcess/Process.cpp
+++ Source/WPEProcess/Process.cpp
@@ -170,7 +170,7 @@ namespace Process {
     class ConsoleOptions : public Core::Options {
     public:
         ConsoleOptions(int argumentCount, TCHAR* arguments[])
-            : Core::Options(argumentCount, arguments, _T("h:l:c:C:r:p:s:d:a:m:i:u:g:t:e:x:V:v:P:L:"))
+            : Core::Options(argumentCount, arguments, _T("h:l:c:C:r:p:s:d:a:m:i:u:g:t:e:x:V:v:P:"))
             , Locator(nullptr)
             , ClassName(nullptr)
             , Callsign(nullptr)
@@ -185,7 +185,6 @@ namespace Process {
             , AppPath()
             , ProxyStubPath()
             , PostMortemPath()
-            , LinkLoaderPath()
             , User(nullptr)
             , Group(nullptr)
             , Threads(1)
@@ -212,7 +211,6 @@ namespace Process {
         string AppPath;
         string ProxyStubPath;
         string PostMortemPath;
-        string LinkLoaderPath;
         const TCHAR* User;
         const TCHAR* Group;
         uint8_t Threads;
@@ -266,9 +264,6 @@ namespace Process {
                 break;
             case 'm':
                 ProxyStubPath = Strip(argument);
-                break;
-            case 'L':
-                LinkLoaderPath = Strip(argument);
                 break;
             case 'u':
                 User = argument;
@@ -582,7 +577,6 @@ int main(int argc, char** argv)
         printf("        [-m <proxy stub library path>]\n");
         printf("        [-e <enabled SYSLOG categories>]\n");
         printf("        [-P <post mortem path>]\n");
-        printf("        [-L <dynamic linker search path>]\n\n");
         printf("This application spawns a seperate process space for a plugin. The plugins");
         printf("are searched in the same order as they are done in process. Starting from:\n");
         printf(" 1) <persistent path>/<locator>\n");
@@ -620,18 +614,6 @@ int main(int argc, char** argv)
                 { return succeeded; },
             NULL, true, -1);
         #endif
-
-        if (options.LinkLoaderPath.empty() == false) {
-            string path;
-
-            // Seems like a LD_LIBRARY_PATH has been passsed to us...
-            if (Core::SystemInfo::GetEnvironment(_T("LD_LIBRARY_PATH"), path) == false) {
-                Core::SystemInfo::SetEnvironment(_T("LD_LIBRARY_PATH"), options.LinkLoaderPath, true);
-            }
-            else {
-                Core::SystemInfo::SetEnvironment(_T("LD_LIBRARY_PATH"), options.LinkLoaderPath + ':' + path, true);
-            }
-        }
 
         Process::ProcessFlow process;
 
