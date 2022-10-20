b669ac0ec03028fadc46231c0af3663a69758e72
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-10-20 09:30:45 +0200

    Core: Add PriviligedRequest

---
+++ Source/core/PriviligedRequest.h
@@ -0,0 +1,360 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2022 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+// #define MODULE_NAME PriviligedRequest
+#include <core/core.h>
+
+namespace WPEFramework {
+
+namespace Core {
+    class PriviligedRequest {
+    private:
+        class Connection : public Core::IResource {
+        private:
+            enum state : uint8_t {
+                IDLE,
+                REQUEST,
+                LISTENING
+            };
+
+        public:
+            Connection() = delete;
+            Connection(const Connection&) = delete;
+            Connection& operator=(const Connection&) = delete;
+
+            Connection(PriviligedRequest& parent)
+                : _parent(parent)
+                , _state(IDLE)
+                , _domainSocket(-1)
+                , _id(~0)
+                , _descriptor(-1)
+                , _signal(true, true)
+            {
+            }
+            ~Connection() override = default;
+
+        public:
+            uint32_t Request(const uint32_t waitTime, const string& connector, const uint32_t id, int& descriptor)
+            {
+                uint32_t result = Core::ERROR_INPROGRESS;
+                state expected = state::IDLE;
+
+                if (_state.compare_exchange_strong(expected, state::REQUEST) == true) {
+                    ASSERT(_domainSocket == -1);
+
+                    result = Core::ERROR_UNAVAILABLE;
+
+                    _domainSocket = ::socket(AF_UNIX, SOCK_DGRAM, 0);
+
+                    constexpr int queue_size = 1;
+
+                    if (_domainSocket >= 0) {
+                        int flags = fcntl(_domainSocket, F_GETFL, 0) | O_NONBLOCK;
+
+                        if (::fcntl(_domainSocket, F_SETFL, flags) != 0) {
+                            TRACE_L1("Error on port socket F_SETFL call. Error: %s", strerror(errno));
+                            result = Core::ERROR_BAD_REQUEST;
+                        } else {
+                            result = Core::ERROR_TIMEDOUT;
+                            _id = id;
+
+                            // Prepare Client
+                            const string clientPath = connector + "-client";
+
+                            ::unlink(clientPath.c_str());
+
+                            struct sockaddr_un clientAddress;
+
+                            ::memset(&clientAddress, 0, sizeof(clientAddress));
+                            clientAddress.sun_family = AF_UNIX;
+                            strncpy(clientAddress.sun_path, clientPath.c_str(), sizeof(clientAddress.sun_path));
+
+                            Address(connector);
+
+                            if (::bind(_domainSocket, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == 0) {
+                                result = Core::ERROR_NONE;
+
+                                ResourceMonitor::Instance().Register(*this);
+                                
+                                _signal.ResetEvent();
+
+                                ::sendto(_domainSocket, &id, sizeof(id), 0, reinterpret_cast<struct sockaddr*>(&_server), sizeof(_server));
+
+                                if (_signal.Lock(waitTime) == Core::ERROR_NONE) {
+                                    descriptor = _descriptor;
+                                    result = Core::ERROR_NONE;
+                                }
+
+                                _descriptor = -1;
+
+                                ResourceMonitor::Instance().Unregister(*this);
+                            } else {
+                                TRACE_L1("Bind to \"%s\" failed. Error: %s", clientPath.c_str(), strerror(errno));
+                                result = Core::ERROR_BAD_REQUEST;
+                            }
+                        }
+
+                        ::close(_domainSocket);
+                        _domainSocket = -1;
+                    } else {
+                        TRACE_L1("failed to open domain socket: %s\n", connector.c_str());
+                    }
+
+                    _state = state::IDLE;
+                }
+                return (result);
+            }
+            uint32_t Open(const string& connector)
+            {
+                uint32_t result = Core::ERROR_ILLEGAL_STATE;
+                state expected = state::IDLE;
+
+                if (_state.compare_exchange_strong(expected, state::LISTENING) == true) {
+                    ASSERT(_domainSocket == -1);
+
+                    result = Core::ERROR_UNAVAILABLE;
+
+                    _domainSocket = ::socket(AF_UNIX, SOCK_DGRAM | SOCK_CLOEXEC, 0);
+
+                    if (_domainSocket >= 0) {
+                        int flags = ::fcntl(_domainSocket, F_GETFL, 0) | O_NONBLOCK;
+
+                        if (::fcntl(_domainSocket, F_SETFL, flags) != 0) {
+                            TRACE_L1("Error on port socket F_SETFL call. Error: %s", strerror(errno));
+                            result = Core::ERROR_BAD_REQUEST;
+                        } else {
+                            ::unlink(connector.c_str());
+
+                            Address(connector);
+
+                            if (::bind(_domainSocket, reinterpret_cast<struct sockaddr*>(&_server), sizeof(_server)) == 0) {
+                                result = Core::ERROR_NONE;
+                                ResourceMonitor::Instance().Register(*this);
+                            } else {
+                                TRACE_L1("Error on port socket bind call. Error: %s", strerror(errno));
+                                result = Core::ERROR_BAD_REQUEST;
+                            }
+                        }
+                    } else {
+                        TRACE_L1("failed to open domain socket: %s\n", connector.c_str());
+                    }
+
+                    if (result != Core::ERROR_NONE) {
+                        if (_domainSocket >= 0) {
+                            TRACE_L1("Closing socket on error: 0x%04X", result);
+                            ::close(_domainSocket);
+                            _domainSocket = -1;
+                        }
+                        _state = state::IDLE;
+                    } else {
+                        TRACE_L1("Server running on fd=%d connector=%s", _domainSocket, connector.c_str());
+                    }
+                }
+                return (result);
+            }
+
+            uint32_t Close()
+            {
+                if (_domainSocket != -1) {
+                    ResourceMonitor::Instance().Unregister(*this);
+                    ::close(_domainSocket);
+                    _domainSocket = -1;
+                }
+                _state = state::IDLE;
+                return (Core::ERROR_NONE);
+            }
+            handle Descriptor() const override
+            {
+                return (_domainSocket);
+            }
+            uint16_t Events() override
+            {
+                return (POLLIN);
+            }
+            void Handle(const uint16_t events) override
+            {
+                if ((events & POLLIN) != 0) {
+                    Read();
+                }
+            }
+
+        private:
+            void Address(const string identifier)
+            {
+                ::memset(&_server, 0, sizeof(_server));
+                _server.sun_family = AF_UNIX;
+                strncpy(_server.sun_path, identifier.c_str(), sizeof(_server.sun_path));
+            }
+            uint32_t Write(const uint32_t id, int fd, struct sockaddr& client, socklen_t length)
+            {
+                uint32_t result = Core::ERROR_NONE;
+
+                struct msghdr msg = { 0 };
+
+                char buf[CMSG_SPACE(sizeof(fd))];
+                memset(buf, 0, sizeof(buf));
+
+                uint32_t identifier(id);
+                struct iovec io = { .iov_base = &identifier, .iov_len = sizeof(identifier) };
+
+                msg.msg_name = &client;
+                msg.msg_namelen = length;
+                msg.msg_iov = &io;
+                msg.msg_iovlen = 1;
+                msg.msg_control = buf;
+                msg.msg_controllen = sizeof(buf);
+
+                struct cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+                cmsg->cmsg_level = SOL_SOCKET;
+                cmsg->cmsg_type = SCM_RIGHTS;
+                cmsg->cmsg_len = CMSG_LEN(sizeof(fd));
+
+                memmove(CMSG_DATA(cmsg), &fd, sizeof(fd));
+
+                msg.msg_controllen = CMSG_SPACE(sizeof(fd));
+
+                ASSERT(_domainSocket != -1);
+
+                int sendBytes = sendmsg(_domainSocket, &msg, 0);
+
+                if (sendBytes < 0) {
+                    TRACE_L1("Error on port socket sendmsg call. Error: %s", strerror(errno));
+                    result = Core::ERROR_BAD_REQUEST;
+                }
+
+                return result;
+            }
+            uint32_t Read()
+            {
+                ASSERT(_domainSocket != -1);
+
+                uint32_t result = Core::ERROR_NONE;
+
+                struct msghdr msg;
+                ::memset(&msg, 0, sizeof(msg));
+
+                if (_state.load(Core::memory_order::memory_order_relaxed) == state::LISTENING) {
+                    uint32_t id;
+                    struct sockaddr_un client;
+
+                    socklen_t clientLen = sizeof(client);
+
+                    int recvBytes = recvfrom(_domainSocket, &id, sizeof(id), 0, reinterpret_cast<sockaddr*>(&client), &clientLen);
+
+                    if (recvBytes < 0) {
+                        TRACE_L1("Error on port socket recvfrom call. Error: %s", strerror(errno));
+                        result = Core::ERROR_UNAVAILABLE;
+                    } else {
+                        int fd = _parent.Service(id);
+                        Write(id, fd, reinterpret_cast<sockaddr&>(client), clientLen);
+                    }
+                } else {
+                    char buf[CMSG_SPACE(sizeof(int))];
+                    memset(buf, 0, sizeof(buf));
+
+                    uint32_t identifier;
+                    struct iovec io = { .iov_base = &identifier, .iov_len = sizeof(identifier) };
+
+                    msg.msg_name = NULL;
+                    msg.msg_namelen = 0;
+                    msg.msg_iov = &io;
+                    msg.msg_iovlen = 1;
+                    msg.msg_control = buf;
+                    msg.msg_controllen = sizeof(buf);
+
+                    int recvBytes = recvmsg(_domainSocket, &msg, 0);
+
+                    if (recvBytes < 0) {
+                        TRACE_L1("Error on port socket recvmsg call. Error: %s", strerror(errno));
+                        result = Core::ERROR_UNAVAILABLE;
+                    } else {
+                        struct cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+
+                        if ((cmsg != nullptr) && (cmsg->cmsg_len == CMSG_LEN(sizeof(int)))) {
+                            if (cmsg->cmsg_level != SOL_SOCKET) {
+                                result = Core::ERROR_BAD_REQUEST;
+                            } else if (cmsg->cmsg_type != SCM_RIGHTS) {
+                                result = Core::ERROR_GENERAL;
+                            } else {
+                                _descriptor = *((int*)CMSG_DATA(cmsg));
+                                _id = identifier;
+                                _signal.SetEvent();
+                            }
+                        } else {
+                            TRACE_L1("Error invalid cmsg %p ", cmsg);
+                        }
+                    }
+                }
+                return result;
+            }
+
+        private:
+            PriviligedRequest& _parent;
+            std::atomic<state> _state;
+            int _domainSocket;
+            uint32_t _id;
+            int _descriptor;
+            Core::Event _signal;
+            struct sockaddr_un _server;
+        };
+
+    public:
+        PriviligedRequest(const PriviligedRequest&) = delete;
+        PriviligedRequest& operator=(const PriviligedRequest&) = delete;
+
+        PriviligedRequest()
+            : _link(*this)
+        {
+        }
+        virtual ~PriviligedRequest()
+        {
+            Close();
+        };
+
+    public:
+        uint32_t Open(const string& identifier)
+        {
+            return (_link.Open(identifier));
+        }
+        uint32_t Close()
+        {
+            return (_link.Close());
+        }
+        Core::IResource::handle Request(const uint32_t waitTime, const string& identifier, const uint32_t requestId)
+        {
+            Core::IResource::handle result = -1;
+            if (_link.Request(waitTime, identifier, requestId, result) != Core::ERROR_NONE) {
+                TRACE_L1("Could not get a priviliged request answered.");
+            }
+            return (result);
+        }
+
+    private:
+        virtual int Service(const uint32_t id)
+        {
+            return -1;
+        }
+
+    private:
+        Connection _link;
+    };
+}
+} // Namespace WPEFramework::Core
