b29f6c6bdec4d1161424a60f719a9f761974e3d3
sebaszm <s.szczepaniak@metrological.com>
2022-12-05 11:39:12 +0100

    [JsonGen] Introduce semi-auto mode for .json meta

---
+++ Tools/JsonGenerator/source/class_emitter.py
@@ -0,0 +1,337 @@
+# Copyright 2020 Metrological
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import config
+import trackers
+from json_loader import *
+
+def EmitEnumRegs(root, emit, header_file, if_file):
+    def _EmitEnumRegistration(root, enum):
+        name = enum.original_type if enum.original_type else (Scoped(root, enum) + enum.cpp_class)
+
+        emit.Line("ENUM_CONVERSION_BEGIN(%s)" % name)
+        emit.Indent()
+
+        for i, _ in enumerate(enum.enumerators):
+            emit.Line("{ %s::%s, _TXT(\"%s\") }," % (name, enum.cpp_enumerators[i], enum.enumerators[i]))
+
+        emit.Unindent()
+        emit.Line("ENUM_CONVERSION_END(%s);" % name)
+
+    emit.Line()
+    emit.Line("// Enumeration code for %s JSON-RPC API." % root.info["title"].replace("Plugin", "").strip())
+    emit.Line("// Generated automatically from '%s'." % os.path.basename(if_file))
+    emit.Line()
+
+    # Enumeration conversion code
+    emit.Line("#include \"definitions.h\"")
+
+    if if_file.endswith(".h"):
+        emit.Line("#include <%s%s>" % (config.CPP_INTERFACE_PATH, if_file))
+
+    emit.Line("#include <core/Enumerate.h>")
+    emit.Line("#include \"%s_%s.h\"" % (config.DATA_NAMESPACE, header_file))
+    emit.Line()
+    emit.Line("namespace %s {" % config.FRAMEWORK_NAMESPACE)
+    count = 0
+
+    for obj in trackers.enum_tracker.objects:
+        if not obj.is_duplicate and not obj.included_from:
+            emit.Line()
+            _EmitEnumRegistration(root, obj)
+            count += 1
+
+    emit.Line()
+    emit.Line("}")
+
+    return count
+
+
+def EmitObjects(log, root, emit, if_file, additional_includes, emitCommon = False):
+    global emittedItems
+    emittedItems = 0
+
+    def _EmitEnumConversionHandler(root, enum):
+        name = enum.original_type if enum.original_type else (Scoped(root, enum) + enum.cpp_class)
+        emit.Line("ENUM_CONVERSION_HANDLER(%s);" % name)
+
+    def _EmitEnum(enum):
+        global emittedItems
+        emittedItems += 1
+
+        log.Info("Emitting enum {}".format(enum.cpp_class))
+
+        if enum.description:
+            emit.Line("// " + enum.description)
+
+        emit.Line("enum%s %s : uint%i_t {" % (" class" if enum.is_scoped else "", enum.cpp_class, enum.size))
+        emit.Indent()
+
+        for c, item in enumerate(enum.cpp_enumerators):
+            emit.Line("%s%s%s" % (item.upper(),
+                                  (" = " + str(enum.cpp_enumerator_values[c])) if enum.cpp_enumerator_values else "",
+                                  "," if not c == len(enum.cpp_enumerators) - 1 else ""))
+
+        emit.Unindent()
+        emit.Line("};")
+        emit.Line()
+
+    def _EmitClass(json_obj, allow_duplicates=False):
+        def EmitInit(json_object):
+            for prop in json_obj.properties:
+                emit.Line("Add(%s, &%s);" % (Tstring(prop.json_name), prop.cpp_name))
+
+        def EmitCtor(json_obj, no_init_code=False, copy_ctor=False, conversion_ctor=False):
+            if copy_ctor:
+                emit.Line("%s(const %s& _other)" % (json_obj.cpp_class, json_obj.cpp_class))
+            elif conversion_ctor:
+                emit.Line("%s(const %s& _other)" % (json_obj.cpp_class, json_obj.cpp_native_type))
+            else:
+                emit.Line("%s()" % (json_obj.cpp_class))
+
+            emit.Indent()
+            emit.Line(": %s()" % CoreJson("Container"))
+            for prop in json_obj.properties:
+                if copy_ctor:
+                    emit.Line(", %s(_other.%s)" % (prop.cpp_name, prop.cpp_name))
+                elif prop.cpp_def_value != '""' and prop.cpp_def_value != "":
+                    emit.Line(", %s(%s)" % (prop.cpp_name, prop.cpp_def_value))
+            emit.Unindent()
+            emit.Line("{")
+            emit.Indent()
+
+            if conversion_ctor:
+                for prop in json_obj.properties:
+                    emit.Line("%s = _other.%s;" % (prop.cpp_name, prop.actual_name))
+
+            if no_init_code:
+                emit.Line("_Init();")
+            else:
+                EmitInit(json_obj)
+
+            emit.Unindent()
+            emit.Line("}")
+
+        def _EmitAssignmentOperator(json_obj, copy_ctor=False, conversion_ctor=False):
+            if copy_ctor:
+                emit.Line("%s& operator=(const %s& _rhs)" % (json_obj.cpp_class, json_obj.cpp_class))
+            elif conversion_ctor:
+                emit.Line("%s& operator=(const %s& _rhs)" % (json_obj.cpp_class, json_obj.cpp_native_type))
+
+            emit.Line("{")
+            emit.Indent()
+
+            for prop in json_obj.properties:
+                if copy_ctor:
+                    emit.Line("%s = _rhs.%s;" % (prop.cpp_name, prop.cpp_name))
+                elif conversion_ctor:
+                    emit.Line("%s = _rhs.%s;" % (prop.cpp_name, prop.actual_name))
+
+            emit.Line("return (*this);")
+            emit.Unindent()
+            emit.Line("}")
+
+        def _EmitConversionOperator(json_obj):
+            emit.Line("operator %s() const" % (json_obj.cpp_native_type))
+            emit.Line("{")
+            emit.Indent();
+            emit.Line("%s _value{};" % (json_obj.cpp_native_type))
+
+            for prop in json_obj.properties:
+                emit.Line("_value.%s = %s;" % ( prop.actual_name, prop.cpp_name))
+
+            emit.Line("return (_value);")
+            emit.Unindent()
+            emit.Line("}")
+
+        # Bail out if a duplicated class!
+        if isinstance(json_obj, JsonObject) and not json_obj.properties:
+            return
+
+        if json_obj.is_duplicate or (not allow_duplicates and json_obj.RefCount() > 1):
+            return
+
+        if not isinstance(json_obj, (JsonRpcSchema, JsonMethod)):
+            log.Info("Emitting class '{}' (source: '{}')".format(json_obj.cpp_class, json_obj.print_name))
+            emit.Line("class %s : public %s {" % (json_obj.cpp_class, CoreJson("Container")))
+            emit.Line("public:")
+            for enum in json_obj.enums:
+                if (enum.do_create and not enum.is_duplicate and (enum.RefCount() == 1)):
+                    emit.Indent()
+                    _EmitEnum(enum)
+                    emit.Unindent()
+
+            emit.Indent()
+        else:
+            if isinstance(json_obj, JsonMethod):
+                if json_obj.included_from:
+                    return
+
+        # Handle nested classes!
+        for obj in trackers.SortByDependency(json_obj.objects):
+            _EmitClass(obj)
+
+        if not isinstance(json_obj, (JsonRpcSchema, JsonMethod)):
+            global emittedItems
+            emittedItems += 1
+            EmitCtor(json_obj, json_obj.is_copy_ctor_needed or "original_type" in json_obj.schema, False, False)
+            if json_obj.is_copy_ctor_needed:
+                emit.Line()
+                EmitCtor(json_obj, True, True, False)
+                emit.Line()
+                _EmitAssignmentOperator(json_obj, True, False)
+
+            if "original_type" in json_obj.schema:
+                emit.Line()
+                EmitCtor(json_obj, True, False, True)
+                emit.Line()
+                _EmitAssignmentOperator(json_obj, False, True)
+                emit.Line()
+                _EmitConversionOperator(json_obj)
+
+            if json_obj.is_copy_ctor_needed or ("original_type" in json_obj.schema):
+                emit.Unindent()
+                emit.Line()
+                emit.Line("private:")
+                emit.Indent()
+                emit.Line("void _Init()")
+                emit.Line("{")
+                emit.Indent()
+                EmitInit(json_obj)
+                emit.Unindent()
+                emit.Line("}")
+                emit.Line()
+            else:
+                emit.Line()
+                emit.Line("%s(const %s&) = delete;" % (json_obj.cpp_class, json_obj.cpp_class))
+                emit.Line("%s& operator=(const %s&) = delete;" % (json_obj.cpp_class, json_obj.cpp_class))
+                emit.Line()
+
+            emit.Unindent()
+            emit.Line("public:")
+            emit.Indent()
+
+            for prop in json_obj.properties:
+                comment = prop.print_name if isinstance(prop, JsonMethod) else prop.description
+                emit.Line("%s %s;%s" % (prop.short_cpp_type, prop.cpp_name, (" // " + comment) if comment else ""))
+
+            emit.Unindent()
+            emit.Line("}; // class %s" % json_obj.cpp_class)
+            emit.Line()
+
+    count = 0
+
+    if trackers.enum_tracker.objects:
+        count = 0
+        for obj in trackers.enum_tracker.objects:
+            if obj.do_create and not obj.is_duplicate and not obj.included_from:
+                count += 1
+
+    emit.Line()
+    emit.Line("// C++ classes for %s JSON-RPC API." % root.info["title"].replace("Plugin", "").strip())
+    emit.Line("// Generated automatically from '%s'. DO NOT EDIT." % os.path.basename(if_file))
+    emit.Line()
+    emit.Line("// Note: This code is inherently not thread safe. If required, proper synchronisation must be added.")
+    emit.Line()
+
+    emit.Line("#pragma once")
+    emit.Line()
+    emit.Line("#include <core/JSON.h>")
+
+    if if_file.endswith(".h"):
+        emit.Line("#include <%s%s>" % (config.CPP_INTERFACE_PATH, if_file))
+
+    for ai in additional_includes:
+        emit.Line("#include <%s%s>" % (config.CPP_INTERFACE_PATH, os.path.basename(ai)))
+
+    if count:
+        emit.Line("#include <core/Enumerate.h>")
+
+    emit.Line()
+    emit.Line("namespace %s {" % config.FRAMEWORK_NAMESPACE)
+    emit.Line()
+    emit.Line("namespace %s {" % config.DATA_NAMESPACE)
+    emit.Indent()
+    emit.Line()
+
+    if "info" in root.schema and "namespace" in root.schema["info"]:
+        emit.Line("namespace %s {" % root.schema["info"]["namespace"])
+        emit.Indent()
+        emit.Line()
+
+    emit.Line("namespace %s {" % root.json_name)
+    emit.Indent()
+    emit.Line()
+
+    if emitCommon and trackers.enum_tracker.CommonObjects():
+        emittedPrologue = False
+        for obj in trackers.enum_tracker.CommonObjects():
+            if obj.do_create and not obj.is_duplicate and not obj.included_from:
+                if not emittedPrologue:
+                    log.Info("Emitting common enums...")
+                    emit.Line("// Common enums")
+                    emit.Line("//")
+                    emit.Line()
+                    emittedPrologue = True
+                _EmitEnum(obj)
+
+    if emitCommon and trackers.object_tracker.CommonObjects():
+        log.Info("Emitting common classes...")
+        emittedPrologue = False
+        for obj in trackers.object_tracker.CommonObjects():
+            if not obj.included_from:
+                if not emittedPrologue:
+                    emit.Line("// Common classes")
+                    emit.Line("//")
+                    emit.Line()
+                    emittedPrologue = True
+                _EmitClass(obj, True)
+
+    if root.objects:
+        log.Info("Emitting params/result classes...")
+        emit.Line("// Method params/result classes")
+        emit.Line("//")
+        emit.Line()
+        _EmitClass(root)
+
+    emit.Unindent()
+    emit.Line("} // namespace %s" % root.json_name)
+    emit.Line()
+
+    if "info" in root.schema and "namespace" in root.schema["info"]:
+        emit.Unindent()
+        emit.Line("} // namespace %s" % root.schema["info"]["namespace"])
+        emit.Line()
+
+    emit.Unindent()
+    emit.Line("} // namespace %s" % config.DATA_NAMESPACE)
+    emit.Line()
+    emittedPrologue = False
+
+    for obj in trackers.enum_tracker.objects:
+        if not obj.is_duplicate and not obj.included_from:
+            if not emittedPrologue:
+                emit.Line("// Enum conversion handlers")
+                emittedPrologue = True
+
+            _EmitEnumConversionHandler(root, obj)
+            emittedItems += 1
+
+    emit.Line()
+    emit.Line("}")
+    emit.Line()
+
+    return emittedItems
---
+++ Tools/JsonGenerator/source/config.py
@@ -0,0 +1,289 @@
+# If not stated otherwise in this file or this component's license file the
+# following copyright and licenses apply:
+#
+# Copyright 2020 Metrological
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import posixpath
+import argparse
+from enum import Enum
+
+# Constants
+DATA_NAMESPACE = "JsonData"
+PLUGIN_NAMESPACE = "Plugin"
+TYPE_PREFIX = "Core::JSON"
+OBJECT_SUFFIX = "Data"
+COMMON_OBJECT_SUFFIX = "Info"
+ENUM_SUFFIX = "Type"
+IMPL_ENDPOINT_PREFIX = "endpoint_"
+IMPL_EVENT_PREFIX = "event_"
+
+# Configurables
+CLASSNAME_FROM_REF = True
+DEFAULT_INT_SIZE = 32
+SHOW_WARNINGS = True
+DOC_ISSUES = True
+DEFAULT_DEFINITIONS_FILE = "../../ProxyStubGenerator/default.h"
+FRAMEWORK_NAMESPACE = "WPEFramework"
+INTERFACE_NAMESPACE = FRAMEWORK_NAMESPACE + "::Exchange"
+INTERFACES_SECTION = True
+INTERFACE_SOURCE_LOCATION = None
+INTERFACE_SOURCE_REVISION = None
+DEFAULT_INTERFACE_SOURCE_REVISION = "main"
+GLOBAL_DEFINITIONS = "../global.json"
+INDENT_SIZE = 4
+ALWAYS_EMIT_COPY_CTOR = False
+KEEP_EMPTY = False
+CPP_INTERFACE_PATH = "interfaces" + os.sep
+JSON_INTERFACE_PATH = CPP_INTERFACE_PATH + "json"
+DUMP_JSON = False
+FORCE = False
+GENERATED_JSON = False
+
+class RpcFormat(Enum):
+    COMPLIANT = "compliant"
+    EXTENDED = "uncompliant-extended"
+    COLLAPSED = "uncompliant-collapsed"
+
+RPC_FORMAT = RpcFormat.COMPLIANT
+RPC_FORMAT_FORCED = False
+
+
+def Parse(cmdline):
+    global DEFAULT_DEFINITIONS_FILE
+    global INTERFACE_NAMESPACE
+    global JSON_INTERFACE_PATH
+    global DEFAULT_INT_SIZE
+    global INDENT_SIZE
+    global DOC_ISSUES
+    global INTERFACE_SOURCE_LOCATION
+    global INTERFACE_SOURCE_REVISION
+    global INTERFACES_SECTION
+    global FORCE
+    global DUMP_JSON
+    global RPC_FORMAT_FORCED
+    global RPC_FORMAT
+    global NO_DUP_WARNINGS
+    global ALWAYS_EMIT_COPY_CTOR
+    global KEEP_EMPTY
+    global CLASSNAME_FROM_REF
+
+    argparser = argparse.ArgumentParser(
+        description='Generate JSON C++ classes, stub code and API documentation from JSON definition files and C++ header files',
+        epilog="For information about custom tags supprted in C++ code please see StubGenerator help (--help-tags).",
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    argparser.add_argument('path',
+            nargs="*",
+            help="JSON file(s) and/or C++ header files, wildcards are allowed")
+    argparser.add_argument("-d",
+            "--docs",
+            dest="docs",
+            action="store_true",
+            default=False,
+            help="generate documentation")
+    argparser.add_argument("-c",
+            "--code",
+            dest="code",
+            action="store_true",
+            default=False,
+            help="generate C++ code building JSON classes and complete JSON-RPC functionality (the latter only if applicable)")
+    argparser.add_argument("-s",
+            "--stubs",
+            dest="stubs",
+            action="store_true",
+            default=False,
+            help="generate C++ stub code for JSON-RPC (i.e. J*.j header file to fill in manually)")
+    argparser.add_argument("-o",
+            "--output",
+            dest="output_dir",
+            metavar="DIR",
+            action="store",
+            default=None,
+            help="output directory, absolute path or directory relative to output file (default: output in the same directory as the source file)")
+    argparser.add_argument(
+            "--force",
+            dest="force",
+            action="store_true",
+            default=False,
+            help= "force code generation even if destination appears up-to-date (default: force disabled)")
+
+    json_group = argparser.add_argument_group("JSON parser arguments (optional)")
+    json_group.add_argument("-i",
+            dest="if_dir",
+            metavar="DIR",
+            action="store",
+            type=str,
+            default=None,
+            help=
+            "a directory with JSON API interfaces that will substitute the {interfacedir} tag (default: same directory as source file)")
+    json_group.add_argument("--no-ref-names",
+            dest="no_ref_names",
+            action="store_true",
+            default=False,
+            help="do not derive class names from $refs (default: derive class names from $ref)")
+    json_group.add_argument("--no-duplicates-warnings",
+            dest="no_duplicates_warnings",
+            action="store_true",
+            default=not SHOW_WARNINGS,
+            help="suppress duplicate object warnings (default: show all duplicate object warnings)")
+
+    cpp_group = argparser.add_argument_group("C++ parser arguments (optional)")
+    cpp_group.add_argument("-j",
+            dest="cppif_dir",
+            metavar="DIR",
+            action="store",
+            type=str,
+            default=None,
+            help=
+            "a directory with C++ API interfaces that will substitute the {cppinterfacedir} tag (default: same directory as source file)")
+    cpp_group.add_argument('-I',
+            dest="includePaths",
+            metavar="INCLUDE_DIR",
+            action='append',
+            default=[],
+            type=str,
+            help='add an include path (can be used multiple times)')
+    cpp_group.add_argument("--include",
+            dest="extra_include",
+            metavar="FILE",
+            action="store",
+            default=DEFAULT_DEFINITIONS_FILE,
+            help="include a C++ header file with common types (default: include '%s')" % DEFAULT_DEFINITIONS_FILE)
+    cpp_group.add_argument("--namespace",
+            dest="if_namespace",
+            metavar="NS",
+            type=str,
+            action="store",
+            default=INTERFACE_NAMESPACE,
+            help="set namespace to look for interfaces in (default: %s)" % INTERFACE_NAMESPACE)
+    cpp_group.add_argument("--format",
+            dest="format",
+            type=str,
+            action="store",
+            default="flexible",
+            choices=["default-compliant", "force-compliant", "default-uncompliant-extended", "force-uncompliant-extended", "default-uncompliant-collapsed", "force-uncompliant-collapsed"],
+            help="select JSON-RPC data format (default: default-compliant)")
+
+    data_group = argparser.add_argument_group("C++ output arguments (optional)")
+    data_group.add_argument("-p",
+            "--data-path",
+            dest="if_path",
+            metavar="PATH",
+            action="store",
+            type=str,
+            default=JSON_INTERFACE_PATH,
+            help="relative path for #include'ing JsonData header file (default: 'interfaces/json', '.' for no path)")
+    data_group.add_argument("--copy-ctor",
+            dest="copy_ctor",
+            action="store_true",
+            default=False,
+            help="always emit a copy constructor and assignment operator (default: emit only when needed)")
+    data_group.add_argument("--def-int-size",
+            dest="def_int_size",
+            metavar="SIZE",
+            type=int,
+            action="store",
+            default=DEFAULT_INT_SIZE,
+            help="default integer size in bits (default: %i)" % DEFAULT_INT_SIZE)
+    data_group.add_argument("--indent",
+            dest="indent_size",
+            metavar="SIZE",
+            type=int,
+            action="store",
+            default=INDENT_SIZE,
+            help="code indentation in spaces (default: %i)" % INDENT_SIZE)
+
+    doc_group = argparser.add_argument_group("Documentation output arguments (optional)")
+    doc_group.add_argument("--no-style-warnings",
+            dest="no_style_warnings",
+            action="store_true",
+            default=not DOC_ISSUES,
+            help="suppress documentation issues (default: show all documentation issues)")
+    doc_group.add_argument("--no-interfaces-section",
+            dest="no_interfaces_section",
+            action="store_true",
+            default=False,
+            help="do not include 'Interfaces' section (default: include interface section)")
+    doc_group.add_argument("--source-location",
+            dest="source_location",
+            metavar="LN",
+            type=str,
+            action="store",
+            default=INTERFACE_SOURCE_LOCATION,
+            help="override interface source file location to the link specified")
+    doc_group.add_argument("--source-revision",
+            dest="source_revision",
+            metavar="ID",
+            type=str,
+            action="store",
+            default=None,
+            help="override interface source file revision to the commit id specified")
+
+    ts_group = argparser.add_argument_group("Troubleshooting arguments (optional)")
+    ts_group.add_argument("--verbose",
+            dest="verbose",
+            action="store_true",
+            default=False,
+            help="enable verbose logging")
+    ts_group.add_argument("--keep-empty",
+            dest="keep_empty",
+            action="store_true",
+            default=False,
+            help="keep generated files that have no code")
+    ts_group.add_argument("--dump-json",
+            dest="dump_json",
+            action="store_true",
+            default=False,
+            help="dump the intermediate JSON file created while parsing a C++ header")
+
+
+    args = argparser.parse_args(cmdline[1:])
+
+    DOC_ISSUES = not args.no_style_warnings
+    NO_DUP_WARNINGS = args.no_duplicates_warnings
+    INDENT_SIZE = args.indent_size
+    ALWAYS_EMIT_COPY_CTOR = args.copy_ctor
+    KEEP_EMPTY = args.keep_empty
+    CLASSNAME_FROM_REF = not args.no_ref_names
+    DEFAULT_INT_SIZE = args.def_int_size
+    DUMP_JSON = args.dump_json
+    FORCE = args.force
+    DEFAULT_DEFINITIONS_FILE = args.extra_include
+    INTERFACE_NAMESPACE = "::" + args.if_namespace if args.if_namespace.find("::") != 0 else args.if_namespace
+    INTERFACES_SECTION = not args.no_interfaces_section
+    INTERFACE_SOURCE_LOCATION = args.source_location
+    INTERFACE_SOURCE_REVISION = args.source_revision
+
+    if RpcFormat.EXTENDED.value in args.format:
+        RPC_FORMAT = RpcFormat.EXTENDED
+    elif RpcFormat.COLLAPSED.value in args.format:
+        RPC_FORMAT = RpcFormat.COLLAPSED
+    else:
+        RPC_FORMAT = RpcFormat.COMPLIANT
+
+    if "force" in args.format:
+        RPC_FORMAT_FORCED = True
+
+    if args.if_path and args.if_path != ".":
+        JSON_INTERFACE_PATH = args.if_path
+    JSON_INTERFACE_PATH = posixpath.normpath(JSON_INTERFACE_PATH) + os.sep
+
+    if args.if_dir:
+        args.if_dir = os.path.abspath(os.path.normpath(args.if_dir))
+    if args.cppif_dir:
+        args.cppif_dir = os.path.abspath(os.path.normpath(args.cppif_dir))
+
+    return argparser, args
