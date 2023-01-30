e8de78632d089ba819f3b5bdac16c890d26e9167
Adrian Muzyka <adrian.muzyka@consult.red>
2023-01-30 14:41:40 +0100

    Added ERROR_INPROC, timeout from request, logs and comments

--- Source/WPEFramework/PluginServer.cpp
+++ Source/WPEFramework/PluginServer.cpp
@@ -680,12 +680,13 @@ namespace PluginHost
         Lock();
 
         IShell::state currentState(State());
+        const string className(PluginHost::Service::Configuration().ClassName.Value());
 
         if (currentState != IShell::state::ACTIVATED) {
             result = Core::ERROR_ILLEGAL_STATE;
         }
         else if (_connection == nullptr) {
-            result = Core::ERROR_BAD_REQUEST;
+            result = Core::ERROR_INPROC;
         }
         else {
             // Oke we have an Connection so there is something to Hibernate..
@@ -701,7 +702,10 @@ namespace PluginHost
                 result = Core::ERROR_NONE;
                 #endif
                 if (result == Core::ERROR_NONE) {
+                    const string className(PluginHost::Service::Configuration().ClassName.Value());
+                    const string callSign(PluginHost::Service::Configuration().Callsign.Value());
                     State(IShell::state::HIBERNATED);
+                    SYSLOG(Logging::Notification, ("Hibernated plugin [%s]:[%s]", className.c_str(), callSign.c_str()));
                 }
                 local->Release();
             }
@@ -739,7 +743,10 @@ namespace PluginHost
                 result = Core::ERROR_NONE;
                 #endif
                 if (result == Core::ERROR_NONE) {
+                    const string className(PluginHost::Service::Configuration().ClassName.Value());
+                    const string callSign(PluginHost::Service::Configuration().Callsign.Value());
                     State(ACTIVATED);
+                    SYSLOG(Logging::Notification, ("Activated plugin from hibernation [%s]:[%s]", className.c_str(), callSign.c_str()));
                 }
                 local->Release();
             }
