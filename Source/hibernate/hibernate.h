3235915dde1a0bd984085e31bdf9c6fc1789ebb9
Pierre Wielders <pierre@wielders.net>
2023-02-01 10:56:00 +0100

    Make the interface agnostic to the real implementation, just offer a pointer that can be hooked up to anything to carry data from Hibernate to Wakeup

--- Source/hibernate/hibernate.h
+++ Source/hibernate/hibernate.h
@@ -8,21 +8,8 @@
 extern "C" {
 #endif
 
-
-struct vm_skip_addr {
-    void* addr;
-    char desc;
-} __attribute__((packed));
-
-struct state_store {
-    int                 nr_threads;
-    pid_t               parasite_pid;
-    pid_t               tids[1024];
-    struct vm_skip_addr skip_addr[1024];
-};
-
-uint32_t Hibernate(const uint32_t timeout, const pid_t pid, const char data_dir[], const char volatile_dir[], struct state_store** storage);
-uint32_t Wakeup(const uint32_t timeout, const pid_t pid, const char data_dir[], const char volatile_dir[], struct state_store** storage);
+uint32_t Hibernate(const uint32_t timeout, const pid_t pid, const char data_dir[], const char volatile_dir[], void** storage);
+uint32_t Wakeup(const uint32_t timeout, const pid_t pid, const char data_dir[], const char volatile_dir[], void** storage);
                  
 #ifdef __cplusplus
 }
