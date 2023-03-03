1ffc50f9cf310e94b7efc44da903efd799cb8a9a
VeithMetro <121170681+VeithMetro@users.noreply.github.com>
2023-03-03 10:13:23 +0100

    Enabling Core_Time.TimeHandle

--- Tests/unit/core/test_time.cpp
+++ Tests/unit/core/test_time.cpp
@@ -2088,7 +2088,6 @@ TEST(Core_Time, OperatorOverloadingSubtractionWithAssignment)
     (currentZone == nullptr) ? unsetenv("TZ") : setenv("TZ", currentZone, 1);
     tzset();
 }
-#if 0
 TEST(Core_Time, TimeHandle)
 {
     Time time(2021,1, 1, 12, 00, 00, 333, true);
@@ -2115,4 +2114,3 @@ TEST(Core_Time, TimeHandle)
     EXPECT_EQ(static_cast<uint32_t>(time.TMHandle().tm_min), 0u);
     EXPECT_EQ(static_cast<uint32_t>(time.TMHandle().tm_sec), 0u);
 }
-#endif
