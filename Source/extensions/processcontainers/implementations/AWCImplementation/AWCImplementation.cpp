4d4468e5513b7b1f6800eb09f45c98908c4b8851
Shelmy Shabu <sshabu.ext@libertyglobal.com>
2024-01-24 16:08:58 +0530

    adding license headers to AWCImplementation.cpp and AWCImplementation.h

--- Source/extensions/processcontainers/implementations/AWCImplementation/AWCImplementation.cpp
+++ Source/extensions/processcontainers/implementations/AWCImplementation/AWCImplementation.cpp
@@ -1,15 +1,14 @@
-/*
- * If not stated otherwise in this file or this component's LICENSE file the
+/* If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
- *
- * Copyright 2020 Metrological
- *
+ * 
+ * Copyright 2021 Liberty Global Service B.V.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
