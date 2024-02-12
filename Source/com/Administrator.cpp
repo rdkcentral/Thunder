1c9aece1d3d0321bd3b8cfd4f472442eaee59c77
MFransen69 <39826971+MFransen69@users.noreply.github.com>
2024-02-12 17:00:40 +0100

    [Com] Proxy non happy day scenario improvements (#1514)
    * [Com] Proxy non happy day scenario improvements
    
    * [Com] Small improvent and logging
    
    * [com] Remove additional logging
    
    ---------
    
    Co-authored-by: Pierre Wielders <pierre@wielders.net>

--- Source/com/Administrator.cpp
+++ Source/com/Administrator.cpp
@@ -114,7 +114,9 @@ namespace RPC {
                 entry++;
             }
             if (entry != index->second.end()) {
+                Core::IUnknown* unknown = (*entry)->Parent();
                 index->second.erase(entry);
+                unknown->Release();                
                 if (index->second.size() == 0) {
                     _channelProxyMap.erase(index);
                 }
@@ -259,8 +261,9 @@ namespace RPC {
                     // Register it as it is remotely registered :-)
                     _channelProxyMap[channel.operator->()].push_back(result);
 
-                    // This will increment the reference count to 1.
+                    // This will increment the reference count to 2 (one in the ChannelProxyMap and one in the QueryInterface ).
                     interface = result->QueryInterface(id);
+                    ASSERT(interface != nullptr);
 
                 } else {
                     TRACE_L1("Failed to find a Proxy for %d.", id);
@@ -373,12 +376,11 @@ namespace RPC {
                 // interface is released in the same time before we report this interface
                 // to be dead. So lets keep a refernce so we can work on a real object
                 // still. This race condition, was observed by customer testing.
-                if ((*loop)->Invalidate() == true) {
-                    pendingProxies.push_back(*loop);
-                }
+                (*loop)->Invalidate();
 
                 loop++;
             }
+            pendingProxies = std::move(index->second);
             _channelProxyMap.erase(index);
         }
 
