c628be11af07068533d5035e434137863df792cc
msieben <4319079+msieben@users.noreply.github.com>
2024-04-04 10:37:34 +0000

    [Tests/unit/core] : Introduce expected timeout result in 'test_cyclicbuffer_dataexchange'.

--- Tests/unit/core/test_cyclicbuffer_dataexchange.cpp
+++ Tests/unit/core/test_cyclicbuffer_dataexchange.cpp
@@ -34,7 +34,7 @@ namespace Tests {
     std::thread([&](std::promise<bool> completed)                         \
         {   /* Before code that should complete before timeout expires */
 
-#define ASYNC_TIMEOUT_END(MILLISECONDS /* timeout in milliseconds */)                                                                           \
+#define ASYNC_TIMEOUT_END(MILLISECONDS /* timeout in milliseconds */, EXPECTATION /* boolean */)                                                \
             /* After code that should complete timely */                                                                                        \
             /* completed.set_value(true); */                                                                                                    \
             completed.set_value_at_thread_exit(true);                                                                                           \
@@ -42,13 +42,13 @@ namespace Tests {
         , std::move(promise)).detach()                                                                                                          \
     ;                                                                                                                                           \
     bool result = future.wait_for(std::chrono::milliseconds(MILLISECONDS)) != std::future_status::timeout;  /* Task completed before timeout */ \
-    EXPECT_TRUE(result);  /* Task completed before timeout */                                                                                   \
+    EXPECT_TRUE(result == EXPECTATION); /* Task completed before timeout */                                                                     \
     if (!result) {                                                                                                                              \
         TRACE_L1(_T("Error : Stopping unresposive process."));                                                                                  \
         killpg(getpgrp(), SIGUSR1); /* Possible 'unresponsive' system, 'unlock' all related 'child' processes, default action is terminate */   \
     }
 
-TEST(Core_CyclicBuffer, DataExchange)
+TEST(Core_CyclicBuffer, DataExchangeTimeout)
 {
     using namespace WPEFramework::Tests;
 
@@ -59,9 +59,7 @@ TEST(Core_CyclicBuffer, DataExchange)
 
     constexpr char fileName[] = "/tmp/SharedCyclicBuffer";
 
-    //constexpr uint32_t totalRuntime = 10000; // Milliseconds
     constexpr uint32_t totalRuntime = Core::infinite; // Milliseconds
-    //constexpr uint32_t timeout = totalRuntime + 10000; // Milliseconds
     constexpr uint32_t timeout = 10000; // Milliseconds
 
     Process<memoryMappedFileRequestedSize, internalBufferSize, maxChildren> process(fileName);
@@ -75,8 +73,37 @@ TEST(Core_CyclicBuffer, DataExchange)
                   && process.Execute()
                );
 
-    ASYNC_TIMEOUT_END(timeout);
+    ASYNC_TIMEOUT_END(timeout, false /* expect no timeout */);
 }
+
+TEST(Core_CyclicBuffer, DataExchange)
+{
+    using namespace WPEFramework::Tests;
+
+    constexpr uint8_t maxChildren = 1;
+
+    constexpr uint32_t memoryMappedFileRequestedSize = 30;//446;
+    constexpr uint32_t internalBufferSize = 40;//446;
+
+    constexpr char fileName[] = "/tmp/SharedCyclicBuffer";
+
+    constexpr uint32_t totalRuntime = 1000; // Milliseconds
+    constexpr uint32_t timeout = totalRuntime + 10000; // Milliseconds
+
+    Process<memoryMappedFileRequestedSize, internalBufferSize, maxChildren> process(fileName);
+
+    ASYNC_TIMEOUT_BEGIN; // Avoid leaking resources, eg, children
+
+    EXPECT_TRUE(process.SetTotalRuntime(totalRuntime)
+                  && process.SetNumReservedBlocks(2)
+                  && process.SetParentUsers(0, 0) /* 0 extra writer(s), 0 reader(s) */
+                  && process.SetChildUsers(0, 1) /* 1 writer(s), 1 reader(s) */
+                  && process.Execute()
+               );
+
+    ASYNC_TIMEOUT_END(timeout, true /* expect no timeout */);
+}
+
 } // Tests
 } // Core
 } // WPEFramework
