f401fb5b10f5ef550f310d629958d1c4079b11a5
msieben <4319079+msieben@users.noreply.github.com>
2024-04-11 12:52:00 +0200

    [Tests/unit/core] : Build fix 'test_databuffer.cpp'. (#1565)
    CyclicDataBuffer was removed in '81348721c522578ceb25d9de8c98ad5736429341'
    
    Co-authored-by: Pierre Wielders <pierre@wielders.net>

--- Tests/unit/core/test_databuffer.cpp
+++ Tests/unit/core/test_databuffer.cpp
@@ -22,46 +22,21 @@
 #include <gtest/gtest.h>
 #include <core/core.h>
 
-using namespace WPEFramework;
+namespace WPEFramework {
+namespace Core {
+namespace Tests {
 
-TEST(Core_DataBuffer, simpleSet)
+TEST(Core_DataBuffer, simple)
 {
-    uint32_t bufferSize = 10;    
-    uint32_t size;
-    uint8_t* received = new uint8_t[bufferSize + 1];
-    memset(received, 0, (bufferSize + 1));
+    constexpr uint32_t blocksize = 10;
 
-    Core::CyclicDataBuffer<Core::ScopedStorage<10>> buffer;
-    EXPECT_TRUE(buffer.IsEmpty());
-    EXPECT_EQ(buffer.Filled(), 0u);
-    EXPECT_EQ(buffer.Free(), bufferSize);
+    WPEFramework::Core::ScopedStorage<blocksize> storage;
 
-    string data = "abcdefghi";
-    size = buffer.Write((uint8_t*)data.c_str(), data.size());
-    EXPECT_EQ(buffer.Filled(), 9u);
-    EXPECT_EQ(buffer.Free(), 1u);
-    EXPECT_EQ(size, data.size());
+    EXPECT_EQ(storage.Size(), blocksize);
 
-    size = buffer.Read(received, 4);
-    received[size] = '\0';
-    EXPECT_STREQ((char*)received, "abcd");
-    EXPECT_EQ(buffer.Filled(), 5u);
-    EXPECT_EQ(buffer.Free(), 5u);
-    EXPECT_EQ(size, 4u);
-
-    data = "jklmnopq";
-    size = buffer.Write((uint8_t*)data.c_str(), data.size());
-    EXPECT_EQ(buffer.Filled(), 9u);
-    EXPECT_EQ(buffer.Free(), 1u);
-    EXPECT_EQ(size, data.size());
-
-    size = buffer.Read((uint8_t*)received, buffer.Filled());
-    received[size] = '\0';
-    EXPECT_STREQ((char*)received, "ijklmnopq");
-    EXPECT_EQ(buffer.Filled(), 0u);
-    EXPECT_EQ(buffer.Free(), 10u);
-    EXPECT_EQ(size, 9u);
-
-    delete[] received;
-    Core::Singleton::Dispose();
+    EXPECT_NE(storage.Buffer(), nullptr);
 }
+
+} // Tests
+} // Core
+} // WPEFramework
