ea9f769c701fe8f8ab21514b6d585b85d7e6f632
msieben <4319079+msieben@users.noreply.github.com>
2024-06-12 08:29:07 +0000

    [Tests/unit/core] : Add namespace(s) for 'test_dataelement'.

--- Tests/unit/core/test_dataelement.cpp
+++ Tests/unit/core/test_dataelement.cpp
@@ -22,96 +22,101 @@
 #include <gtest/gtest.h>
 #include <core/DataElement.h>
 
-using namespace Thunder;
-using namespace Thunder::Core;
+namespace Thunder {
+namespace Tests {
+    using namespace Thunder;
+    using namespace Thunder::Core;
 
-TEST(test_data, simple_data)
-{
-    const uint8_t arr[] = {10,20,30,40,50,60,70,80,90,100};
-    const uint16_t len = 5;
-    const uint16_t off = 0;
-    DataStore obj1;
-    obj1.Copy(arr,len,off);
-    uint32_t size = 1024;
-    EXPECT_EQ(obj1.Size(),size);
-    obj1.Size(1025);
-    ASSERT_EQ(obj1.Size(), 1025);
-    EXPECT_EQ(*obj1.Buffer(),10);
+    TEST(test_data, simple_data)
+    {
+        const uint8_t arr[] = {10,20,30,40,50,60,70,80,90,100};
+        const uint16_t len = 5;
+        const uint16_t off = 0;
+        DataStore obj1;
+        obj1.Copy(arr,len,off);
+        uint32_t size = 1024;
+        EXPECT_EQ(obj1.Size(),size);
+        obj1.Size(1025);
+        ASSERT_EQ(obj1.Size(), 1025);
+        EXPECT_EQ(*obj1.Buffer(),10);
 
-    uint8_t arr1[] = {10,20,30,40,50,60,70,80,90,100};
-    const uint64_t s = 10;
-    DataElement obj_sample;
-    DataElement obj2(s,arr1);
-    DataElement obj_operator = obj2;
-    uint32_t crc = 4134036294;
-    EXPECT_EQ(obj2.CRC32(0,10),crc);
-    EXPECT_TRUE(obj2.Copy(obj2,0));
+        uint8_t arr1[] = {10,20,30,40,50,60,70,80,90,100};
+        const uint64_t s = 10;
+        DataElement obj_sample;
+        DataElement obj2(s,arr1);
+        DataElement obj_operator = obj2;
+        uint32_t crc = 4134036294;
+        EXPECT_EQ(obj2.CRC32(0,10),crc);
+        EXPECT_TRUE(obj2.Copy(obj2,0));
 
-    uint64_t alloc_size = 10;
-    EXPECT_EQ(obj2.AllocatedSize(), alloc_size);
-    EXPECT_TRUE(obj2.Size(sizeof(arr)));
-    EXPECT_TRUE(obj2.IsValid());
-    EXPECT_EQ(*obj2.Buffer(),10);
+        uint64_t alloc_size = 10;
+        EXPECT_EQ(obj2.AllocatedSize(), alloc_size);
+        EXPECT_TRUE(obj2.Size(sizeof(arr)));
+        EXPECT_TRUE(obj2.IsValid());
+        EXPECT_EQ(*obj2.Buffer(),10);
 
-    uint8_t arr2[] = {10,20,30,40,50};
-    const uint8_t infom = 10;
-    uint64_t SearchNumber = 10;
-    EXPECT_EQ((obj2.SearchNumber<uint8_t, Core::ENDIAN_BIG>(2,infom)),SearchNumber);
-    obj2.SetNumber<uint64_t, Core::ENDIAN_BIG>(2,10);
-    uint64_t GetNumber = 10;
-    EXPECT_EQ((obj2.GetNumber<uint64_t, Core::ENDIAN_BIG>(2)),GetNumber);
+        uint8_t arr2[] = {10,20,30,40,50};
+        const uint8_t infom = 10;
+        uint64_t SearchNumber = 10;
+        EXPECT_EQ((obj2.SearchNumber<uint8_t, Core::ENDIAN_BIG>(2,infom)),SearchNumber);
+        obj2.SetNumber<uint64_t, Core::ENDIAN_BIG>(2,10);
+        uint64_t GetNumber = 10;
+        EXPECT_EQ((obj2.GetNumber<uint64_t, Core::ENDIAN_BIG>(2)),GetNumber);
 
-    EXPECT_EQ(obj2.Search(2,arr2,5), obj2.Size()); // Unable to find pattern
-    obj2.SetBitNumber<uint64_t>(2,5,8,10);
-    uint64_t GetBitNumber = 10;
-    EXPECT_EQ(obj2.GetBitNumber<uint64_t>(2,5,8),GetBitNumber);
+        EXPECT_EQ(obj2.Search(2,arr2,5), obj2.Size()); // Unable to find pattern
+        obj2.SetBitNumber<uint64_t>(2,5,8,10);
+        uint64_t GetBitNumber = 10;
+        EXPECT_EQ(obj2.GetBitNumber<uint64_t>(2,5,8),GetBitNumber);
 
-    const uint8_t val= 32;
-    const uint64_t offset= 0;
-    obj2.Set(val,offset);
-    obj2.Align<uint8_t>();
+        const uint8_t val= 32;
+        const uint64_t offset= 0;
+        obj2.Set(val,offset);
+        obj2.Align<uint8_t>();
 
-    DataElement obj(obj2);
-    DataElement obj3 = obj;
-    DataElement obj4(obj2,0,0);
+        DataElement obj(obj2);
+        DataElement obj3 = obj;
+        DataElement obj4(obj2,0,0);
 
-    uint32_t ob_size = 10;
-    EXPECT_EQ(obj3.Size(),ob_size);
-    EXPECT_FALSE(obj3.Expand(0,0));
-    EXPECT_TRUE(obj3.Shrink(0,0));
-    EXPECT_FALSE(obj3.Copy(obj2));
+        uint32_t ob_size = 10;
+        EXPECT_EQ(obj3.Size(),ob_size);
+        EXPECT_FALSE(obj3.Expand(0,0));
+        EXPECT_TRUE(obj3.Shrink(0,0));
+        EXPECT_FALSE(obj3.Copy(obj2));
+    }
+
+    TEST(test_linkeddata, simple_linkeddata)
+    {
+        uint8_t arr[] = {10,20,30,40,50,60,70,80,90,100};
+        uint8_t arr1[sizeof(arr)] = {};
+        const uint64_t offset= 0;
+        DataElement objt1(10,arr);
+        LinkedDataElement ob1;
+        LinkedDataElement ob2(objt1);
+        LinkedDataElement ob3(ob2);
+        ob3.Enclosed(&ob2);
+        EXPECT_EQ(ob3.Enclosed(), &ob2);
+        ob3.SetBuffer(2,9,arr);
+        ob3.GetBuffer(2,9,arr1);
+        LinkedDataElement ob4;
+        ob4 = ob2;
+        EXPECT_EQ(ob4.Copy(offset,ob2), unsigned(10));
+        EXPECT_EQ(ob2.Copy(offset,ob3), unsigned(10));
+
+        EXPECT_EQ(ob1.Enclosed(), nullptr);
+        EXPECT_EQ(ob2.LinkedSize(), unsigned(10));
+        EXPECT_EQ(ob2.LinkedElements(),unsigned(1));
+    }
+
+    TEST(test_dataParser, simple_dataParser)
+    {
+        uint8_t arr[] = {10,20,30,40,50};
+        DataElement object1(10,arr);
+        DataElementParser parser1(object1,0);
+        uint64_t size = -10;
+        EXPECT_TRUE(parser1.IsValid());
+        EXPECT_EQ(parser1.Size(),size);
+        parser1.SkipBytes(2);
+    }
+
 }
-
-TEST(test_linkeddata, simple_linkeddata)
-{
-    uint8_t arr[] = {10,20,30,40,50,60,70,80,90,100};
-    uint8_t arr1[sizeof(arr)] = {};
-    const uint64_t offset= 0;
-    DataElement objt1(10,arr);
-    LinkedDataElement ob1;
-    LinkedDataElement ob2(objt1);
-    LinkedDataElement ob3(ob2);
-    ob3.Enclosed(&ob2);
-    EXPECT_EQ(ob3.Enclosed(), &ob2);
-    ob3.SetBuffer(2,9,arr);
-    ob3.GetBuffer(2,9,arr1);
-    LinkedDataElement ob4;
-    ob4 = ob2;
-    EXPECT_EQ(ob4.Copy(offset,ob2), unsigned(10));
-    EXPECT_EQ(ob2.Copy(offset,ob3), unsigned(10));
-
-    EXPECT_EQ(ob1.Enclosed(), nullptr);
-    EXPECT_EQ(ob2.LinkedSize(), unsigned(10));
-    EXPECT_EQ(ob2.LinkedElements(),unsigned(1));
-}
-
-TEST(test_dataParser, simple_dataParser)
-{
-    uint8_t arr[] = {10,20,30,40,50};
-    DataElement object1(10,arr);
-    DataElementParser parser1(object1,0);
-    uint64_t size = -10;
-    EXPECT_TRUE(parser1.IsValid());
-    EXPECT_EQ(parser1.Size(),size);
-    parser1.SkipBytes(2);
 }
