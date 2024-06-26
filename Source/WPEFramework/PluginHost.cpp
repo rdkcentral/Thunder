a1f7c2d8e5cd77e917121b5438625fd52891160d
sethulakshmisk <ssk@synamedia.com>
2024-06-26 11:19:59 +0530

    Update PluginHost.cpp
    Updated based on internal review comments

--- Source/WPEFramework/PluginHost.cpp
+++ Source/WPEFramework/PluginHost.cpp
@@ -18,7 +18,7 @@
  */
 
 #include "PluginServer.h"
-#include "../core/Library.h"
+#include "core/Library.h"
 #include <fstream>
 
 #ifndef __WINDOWS__
@@ -353,9 +353,7 @@ POP_WARNING()
         }
     }
 
-namespace {
-
-    void SetupCrashHandler(void)
+    static void SetupCrashHandler(void)
     {
         _adminLock.Lock();
         struct sigaction sa, current_sa;
@@ -397,7 +395,6 @@ namespace {
         }
         _adminLock.Unlock();
     }
-}
 #endif
 
     void LoadPlugins(const string& name, PluginHost::Config& config)
