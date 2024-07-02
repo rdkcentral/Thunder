217e0d39c48924f1f8ab20ae4b8113a54cb5fefe
MFransen69 <39826971+MFransen69@users.noreply.github.com>
2024-07-02 08:39:24 +0200

    Update IShell.h

--- Source/plugins/IShell.h
+++ Source/plugins/IShell.h
@@ -1,5 +1,5 @@
 /*
- * If not stated otherwise in this file or this component's LICENSE file the
+ * If not stated otherwise in this file or this component's LICENSE file the 
  * following copyright and licenses apply:
  *
  * Copyright 2020 Metrological
