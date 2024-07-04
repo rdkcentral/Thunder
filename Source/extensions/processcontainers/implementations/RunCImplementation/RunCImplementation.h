899ba40ac2a49048820e89c291982c151b09f5b9
sebaszm <45654185+sebaszm@users.noreply.github.com>
2024-07-04 16:10:13 +0200

    [Containers] Re-enable runC implementation (#1689)
    Co-authored-by: MFransen69 <39826971+MFransen69@users.noreply.github.com>

--- Source/extensions/processcontainers/implementations/RunCImplementation/RunCImplementation.h
+++ Source/extensions/processcontainers/implementations/RunCImplementation/RunCImplementation.h
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include "processcontainers/Messaging.h"
 #include "processcontainers/ProcessContainer.h"
 #include "processcontainers/common/BaseAdministrator.h"
 #include "processcontainers/common/BaseRefCount.h"
@@ -27,59 +28,62 @@
 namespace Thunder {
 namespace ProcessContainers {
 
+    namespace runc {
+        class Runner;
+    }
+
     class RunCContainer : public BaseRefCount<IContainer> {
     private:
         friend class RunCContainerAdministrator;
-
-        RunCContainer(const string& name, const string& path, const string& logPath);
+        RunCContainer(const string& id, const string& path, const string& logPath);
 
     public:
         RunCContainer(const RunCContainer&) = delete;
         RunCContainer& operator=(const RunCContainer&) = delete;
+        RunCContainer(RunCContainer&&) = delete;
+        RunCContainer& operator=(RunCContainer&&) = delete;
         ~RunCContainer() override;
 
-        // IContainerMethods
+    public:
+        // IContainer methods
         const string& Id() const override;
         uint32_t Pid() const override;
         bool IsRunning() const override;
         bool Start(const string& command, IStringIterator& parameters) override;
-        bool Stop(const uint32_t timeout /*ms*/) override;
-
+        bool Stop(const uint32_t timeout /* ms */) override;
         IMemoryInfo* Memory() const override;
         IProcessorInfo* ProcessorInfo() const override;
         INetworkInterfaceIterator* NetworkInterfaces() const override;
 
     private:
+        uint32_t InternalPurge(const uint32_t timeout = 0);
+
+    private:
         mutable Core::CriticalSection _adminLock;
-        string _name;
+        string _id;
         string _path;
-        string _logPath;
-        mutable Core::OptionalType<uint32_t> _pid;
+        mutable uint32_t _pid;
+        std::unique_ptr<runc::Runner> _runner;
     };
 
     class RunCContainerAdministrator : public BaseContainerAdministrator<RunCContainer> {
     private:
         friend class RunCContainer;
         friend class Core::SingletonType<RunCContainerAdministrator>;
-
-        RunCContainerAdministrator();
-        RunCContainerAdministrator(const RunCContainerAdministrator&) = delete;
-        RunCContainerAdministrator& operator=(const RunCContainerAdministrator&) = delete;
+        RunCContainerAdministrator() = default;
 
     public:
-        ~RunCContainerAdministrator() override;
+        RunCContainerAdministrator(const RunCContainerAdministrator&) = delete;
+        RunCContainerAdministrator(RunCContainerAdministrator&&) = delete;
+        RunCContainerAdministrator& operator=(const RunCContainerAdministrator&) = delete;
+        RunCContainerAdministrator& operator=(RunCContainerAdministrator&&) = delete;
+        ~RunCContainerAdministrator() override = default;
 
-        IContainer* Container(const string& id,
-            IStringIterator& searchpaths,
-            const string& logpath,
-            const string& configuration) override; //searchpaths will be searched in order in which they are iterated
-
+    public:
         // IContainerAdministrator methods
+        IContainer* Container(const string& id, IStringIterator& searchpaths, const string& logpath, const string& configuration) override;
         void Logging(const string& logDir, const string& loggingOptions) override;
-
-    protected:
-        void DestroyContainer(const string& name); // make sure that no leftovers from previous launch will cause crash
-        bool ContainerNameTaken(const string& name);
     };
-}
+
+} // namespace ProcessContainers
 }
