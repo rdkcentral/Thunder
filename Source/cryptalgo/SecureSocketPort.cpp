7916e61feaa02a4a9942203219e350ae2f56990c
msieben <4319079+msieben@users.noreply.github.com>
2024-11-21 13:43:21 +0000

    [Source/crypto/SecureSocketPort] : Context settings do not (necessarily) propagate to SSL structure after construction.
    - Context should exist
    
    - Options should be set

--- Source/cryptalgo/SecureSocketPort.cpp
+++ Source/cryptalgo/SecureSocketPort.cpp
@@ -135,16 +135,21 @@ uint32_t SecureSocketPort::Handler::Initialize() {
 
     _context = SSL_CTX_new(TLS_method());
 
-    _ssl = SSL_new(static_cast<SSL_CTX*>(_context));
-    SSL_set_fd(static_cast<SSL*>(_ssl), static_cast<Core::IResource&>(*this).Descriptor());
-    SSL_CTX_set_options(static_cast<SSL_CTX*>(_context), SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
+    if (_context != nullptr) {
+        if (   // Returns bit-mask after adding options
+               ((SSL_CTX_set_options(static_cast<SSL_CTX*>(_context), SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3) & (SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3))  == (SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3))
+            && // Trust the same certificates as any other application
+               (SSL_CTX_set_default_verify_paths(static_cast<SSL_CTX*>(_context)) == 1)
+           ) {
+            _ssl = SSL_new(static_cast<SSL_CTX*>(_context));
 
-    // Trust the same certificates as any other application
-    if (SSL_CTX_set_default_verify_paths(static_cast<SSL_CTX*>(_context)) == 1) {
-        success = Core::SocketPort::Initialize();
-    } else {
-        TRACE_L1("OpenSSL failed to load certificate store");
-        success = Core::ERROR_GENERAL;
+            SSL_set_fd(static_cast<SSL*>(_ssl), static_cast<Core::IResource&>(*this).Descriptor());
+
+            success = Core::SocketPort::Initialize();
+        } else {
+            TRACE_L1("OpenSSL failed to set protocol level or load certificate store");
+            success = Core::ERROR_GENERAL;
+        }
     }
 
     return success;
