981b3e3c6884aa178f58f4e8b774b6718c74e1f4
msieben <4319079+msieben@users.noreply.github.com>
2025-10-01 13:02:34 +0000

    [core/CyclicBuffer] : match clockid_t for condition variable attribute to used clock

--- Source/core/CyclicBuffer.cpp
+++ Source/core/CyclicBuffer.cpp
@@ -66,6 +66,11 @@ namespace Core {
                 ret = pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
 
+                #ifdef __LINUX__
+                ret = pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC);
+                ASSERT(ret == 0); DEBUG_VARIABLE(ret);
+                #endif
+
                 ret = pthread_cond_init(&(_administration->_signal), &cond_attr);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
 
@@ -155,6 +160,11 @@ namespace Core {
 
                 ret = pthread_cond_init(&(_administration->_signal), &cond_attr);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
+
+                #ifdef __LINUX__
+                ret = pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC);
+                ASSERT(ret == 0); DEBUG_VARIABLE(ret);
+                #endif
 
                 pthread_mutexattr_t mutex_attr;
 
