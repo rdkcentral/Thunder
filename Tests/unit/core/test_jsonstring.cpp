f0e2eefbe5565db1e95ff199d43ecf2f839196f3
msieben <4319079+msieben@users.noreply.github.com>
2025-10-09 15:18:37 +0000

    [Tests/unit/core] : add 'test_jsonstring'

---
+++ Tests/unit/core/test_jsonstring.cpp
@@ -0,0 +1,48 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <functional>
+#include <sstream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#ifndef MODULE_NAME
+#include "../Module.h"
+#endif
+ 
+#include <core/core.h>
+
+namespace Thunder {
+namespace Tests {
+namespace Core {
+
+TEST(JSONString, Failure)
+{
+    std::string textOut{};
+
+    Thunder::Core::JSON::String opaque;
+
+    opaque.FromString(R"json({"payload":"This is test\" message"})json");
+    opaque.ToString(textOut);
+
+    EXPECT_STREQ(textOut.c_str(), R"("{\"payload\":\"This is test\\\" message\"}")");
+}
+
+} } } // Thunder::Tests::Core
