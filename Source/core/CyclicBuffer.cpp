9fbb0871587abb1d41881dd3f4222992a348f35b
msieben <4319079+msieben@users.noreply.github.com>
2025-10-23 14:27:46 +0000

    [core/CyclicBuffer] : fix MacOS build

--- Source/core/CyclicBuffer.cpp
+++ Source/core/CyclicBuffer.cpp
@@ -65,10 +65,10 @@ namespace Core {
 
                 ret = pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-
+#ifndef __APPLE__
                 ret = pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-
+#endif
                 ret = pthread_cond_init(&(_administration->_signal), &cond_attr);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
 
