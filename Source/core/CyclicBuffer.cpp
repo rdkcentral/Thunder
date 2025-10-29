8251e4612dc787ccfe1ef5e63ed00d37139f6e40
Volkan <volkan.aslan@outlook.com>
2025-10-29 15:14:21 +0100

    Revert "Development/metrol 1190 (#1968)"
    This reverts commit fdea42fb401cd098ada6583df5c1be8f22351569.

--- Source/core/CyclicBuffer.cpp
+++ Source/core/CyclicBuffer.cpp
@@ -65,10 +65,7 @@ namespace Core {
 
                 ret = pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-#ifndef __APPLE__
-                ret = pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC);
-                ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-#endif
+
                 ret = pthread_cond_init(&(_administration->_signal), &cond_attr);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
 
@@ -158,11 +155,6 @@ namespace Core {
 
                 ret = pthread_cond_init(&(_administration->_signal), &cond_attr);
                 ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-
-#ifndef __APPLE__
-                ret = pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC);
-                ASSERT(ret == 0); DEBUG_VARIABLE(ret);
-#endif
 
                 pthread_mutexattr_t mutex_attr;
 
