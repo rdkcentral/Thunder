af39e6b07061ac65c11103d710bb7eb9ac43cb8c
VeithMetro <m.daniluk@metrological.com>
2025-11-03 12:04:08 +0100

    Make it possible to distinguish between ThreadPool created in Thunder and ThunderPlugin, bring the ASSERT back

--- Source/ThunderPlugin/Process.cpp
+++ Source/ThunderPlugin/Process.cpp
@@ -111,8 +111,8 @@ POP_WARNING()
         WorkerPoolImplementation& operator=(const WorkerPoolImplementation&) = delete;
 
 PUSH_WARNING(DISABLE_WARNING_THIS_IN_MEMBER_INITIALIZER_LIST)
-        WorkerPoolImplementation(const uint8_t threads, const uint32_t stackSize, const uint32_t queueSize, const string& callsign)
-            : WorkerPool(threads - 1, stackSize, queueSize, &_dispatcher, this, threads, threads)
+        WorkerPoolImplementation(const uint8_t threads, const uint32_t stackSize, const uint32_t queueSize, const string& callsign, const uint8_t aditionalThreads = 1)
+            : WorkerPool(threads - 1, stackSize, queueSize, &_dispatcher, this, (threads > 2 ? (threads - 1) : threads), (threads > 2 ? (threads - 1) : threads), aditionalThreads)
             , _dispatcher(callsign)
             , _sink(*this)
         {
