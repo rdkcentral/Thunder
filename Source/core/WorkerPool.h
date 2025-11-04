9da8f4e0b9d5315ece677be0bd8798adaf5015f9
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2025-11-04 14:24:52 +0100

    [Priority Queue] Distinguish the source of ThreadPool to bring the ASSERT back (#1987)
    * Make it possible to distinguish between ThreadPool created in Thunder and ThunderPlugin, bring the ASSERT back
    
    * Rename the new debug param
    
    * Adjust thread count in WorkerPool constructor
    
    ---------
    
    Co-authored-by: MFransen69 <39826971+MFransen69@users.noreply.github.com>

--- Source/core/WorkerPool.h
+++ Source/core/WorkerPool.h
@@ -315,9 +315,9 @@ namespace Core {
         WorkerPool& operator=(const WorkerPool&) = delete;
 
 PUSH_WARNING(DISABLE_WARNING_THIS_IN_MEMBER_INITIALIZER_LIST)
-        WorkerPool(const uint8_t threadCount, const uint32_t stackSize, const uint32_t queueSize, ThreadPool::IDispatcher* dispatcher, ThreadPool::ICallback* callback = nullptr, const uint16_t lowPriorityThreadCount = 0, const uint16_t mediumPriorityThreadCount = 0)
+        WorkerPool(const uint8_t threadCount, const uint32_t stackSize, const uint32_t queueSize, ThreadPool::IDispatcher* dispatcher, ThreadPool::ICallback* callback = nullptr, const uint16_t lowPriorityThreadCount = 0, const uint16_t mediumPriorityThreadCount = 0, const uint8_t additionalThreads = 0)
             : _scheduler(this, _timer)
-            , _threadPool(threadCount, stackSize, queueSize, dispatcher, &_scheduler, &_external, callback, lowPriorityThreadCount, mediumPriorityThreadCount)
+            , _threadPool(threadCount, stackSize, queueSize, dispatcher, &_scheduler, &_external, callback, lowPriorityThreadCount, mediumPriorityThreadCount, additionalThreads)
             , _external(_threadPool, dispatcher)
             , _timer(1024 * 1024, _T("WorkerPoolType::Timer"))
             , _metadata()
