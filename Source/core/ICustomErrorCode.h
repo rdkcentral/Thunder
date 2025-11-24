7c9ed4cfe1e2ccbb5d1c09bfb80d4be44f3a72b4
MFransen <m.fransen@metrological.com>
2025-11-25 00:17:35 +0100

    [Core] another type in the header file...

--- Source/core/ICustomErrorCode.h
+++ Source/core/ICustomErrorCode.h
@@ -47,7 +47,7 @@ extern "C" {
 #endif
 
 // called from within Thunder to get the string representation for a custom code
-// note parameter code is the pure (signed) Custom Code passed to Thunder. no additional bits set (so for signed numbers 32th bit used as sign bit). 
+// note parameter code is the pure (signed) Custom Code passed to Thunder. no additional bits set (so for signed numbers 32nd bit used as sign bit). 
 // in case no special string representation is needed return nullptr (NULL), in that case Thunder will convert the Custom Code to a generic message itself
 EXTERNAL const TCHAR* CustomCodeToString(const int32_t code);
 
