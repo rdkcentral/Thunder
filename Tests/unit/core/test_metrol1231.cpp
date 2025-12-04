20b91a07260db43bc4bb60c8ee62e3305a58853c
msieben <4319079+msieben@users.noreply.github.com>
2025-12-04 14:22:57 +0000

    [corre/JSON / Tests/unit/core] : METROL-1231

---
+++ Tests/unit/core/test_metrol1231.cpp
@@ -0,0 +1,106 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef __APPLE__
+#include <time.h>
+#endif
+
+#include <gtest/gtest.h>
+
+#ifndef MODULE_NAME
+#include "../Module.h"
+#endif
+
+#include <core/core.h>
+
+namespace Thunder {
+namespace Tests {
+namespace Core {
+
+    TEST(METROL_1231, FirstKeyRemove)
+    {
+        const std::string keyValues = R"({"Name": "Your full name", "Age": 0, "Gender": "Undisclosed"})";
+
+        JsonObject container;
+
+        EXPECT_TRUE(container.FromString(keyValues));
+
+        EXPECT_FALSE(container.HasLabel("name"));
+
+        EXPECT_TRUE(container.HasLabel("Name"));
+
+        /* void */ container.Remove("Name");
+
+        EXPECT_FALSE(container.HasLabel("Name"));
+    }
+
+    TEST(METROL_1231, SecondKeyRemove)
+    {
+        const std::string keyValues = R"({"Name": "Your full name", "Age": 0, "Gender": "Undisclosed"})";
+
+        JsonObject container;
+
+        EXPECT_TRUE(container.FromString(keyValues));
+
+        EXPECT_FALSE(container.HasLabel("age"));
+
+        EXPECT_TRUE(container.HasLabel("Age"));
+
+        /* void */ container.Remove("Age");
+
+        EXPECT_FALSE(container.HasLabel("Age"));
+    }
+
+    TEST(METROL_1231, ThirdKeyRemove)
+    {
+        const std::string keyValues = R"({"Name": "Your full name", "Age": 0, "Gender": "Undisclosed"})";
+
+        JsonObject container;
+
+        EXPECT_TRUE(container.FromString(keyValues));
+
+        EXPECT_FALSE(container.HasLabel("gender"));
+
+        EXPECT_TRUE(container.HasLabel("Gender"));
+
+        /* void */ container.Remove("Gender");
+
+        EXPECT_FALSE(container.HasLabel("Gender"));
+    }
+
+    TEST(METROL_1231, IdenticalKeyValueRemove)
+    {
+        const std::string keyValues = R"({"Name": "Your full name", "Age": "Age", "Gender": "Undisclosed"})";
+
+        JsonObject container;
+
+        EXPECT_TRUE(container.FromString(keyValues));
+
+        EXPECT_FALSE(container.HasLabel("age"));
+
+        EXPECT_TRUE(container.HasLabel("Age"));
+
+        /* void */ container.Remove("Age");
+
+        EXPECT_FALSE(container.HasLabel("Age"));
+    }
+
+} // Core
+} // Tests
+} // Thunder
