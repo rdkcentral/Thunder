da0cc34ee93007b33a79a1a5f9a8bca62683b133
Sivarajappan Siva <sivarajappan_siva@comcast.com>
2026-01-12 09:56:50 +0000

    Porting of PR 2038

--- Source/core/JSON.h
+++ Source/core/JSON.h
@@ -3816,7 +3816,8 @@ namespace Core {
             {
                 JSONElementList::iterator index(_data.begin());
 
-                while ((index != _data.end()) && (index->first != label)) {
+                while ((index != _data.end())
+                       &&(strncmp(index->first, label, std::min(strlen(label), strlen(index->first))) != 0)) {
                     index++;
                 }
 
@@ -4862,7 +4863,8 @@ namespace Core {
 
             bool HasLabel(const TCHAR labelName[]) const
             {
-                return (Find(labelName) != _elements.end());
+                return (Find(labelName) != _elements.end()
+                        && Container::HasLabel(labelName) != false);
             }
 
             Iterator Variants() const
@@ -4876,6 +4878,16 @@ namespace Core {
                 _elements.clear();
             }
             string GetDebugString(int indent = 0) const;
+
+            void Remove(const TCHAR label[])
+            {
+                Elements::iterator index = Find(label);
+                if (index != _elements.end()) {
+                    _elements.erase(index);
+                }
+
+                Container::Remove(label);
+            }
 
         private:
             Elements::iterator Find(const TCHAR fieldName[])
