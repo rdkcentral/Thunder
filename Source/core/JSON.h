e6f4a7045a6118c67eb6e22ea228b5d07f1d6d31
Sivarajappan Siva <sivarajappan_siva@comcast.com>
2026-01-28 05:47:12 +0000

    Porting of PR 2051

--- Source/core/JSON.h
+++ Source/core/JSON.h
@@ -3816,8 +3816,7 @@ namespace Core {
             {
                 JSONElementList::iterator index(_data.begin());
 
-                while ((index != _data.end())
-                       &&(strncmp(index->first, label, std::min(strlen(label), strlen(index->first))) != 0)) {
+                while (index != _data.end() && (strlen(index->first) != strlen(label) || strcmp(index->first, label) != 0)) {
                     index++;
                 }
 
