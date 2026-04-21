f75fe89ce30d78b8229a335ceb43edeca13ffc74
SatheeshkumarG15 <satheeshkumar00150@gmail.com>
2026-04-21 17:32:22 +0530

    Used an atomic bool and compare_exchange_strong to make sure either ResourceMonitor or Workerpool thread to properly clear out the service and to avoid the crash

--- Source/Thunder/PluginServer.h
+++ Source/Thunder/PluginServer.h
@@ -25,6 +25,7 @@
 #include "IRemoteInstantiation.h"
 #include "WarningReportingCategories.h"
 #include "PostMortem.h"
+#include <atomic>
 
 #ifndef HOSTING_COMPROCESS
 #error "Please define the name of the COM process!!!"
@@ -4884,11 +4885,8 @@ namespace PluginHost {
 
                 // If we are closing (or closed) do the clean up
                 if (IsOpen() == false) {
-                    if (_service.IsValid() == true) {
-                        _service->Detach(*this);
 
-                        _service.Release();
-                    }
+                    CleanupService();
 
                     State(CLOSED, false);
 
@@ -4958,11 +4956,24 @@ namespace PluginHost {
                 SetId(id);
             }
 
+            void CleanupService()
+            {
+                bool expected = false;
+                if (_serviceCleanedUp.compare_exchange_strong(expected, true, std::memory_order_acq_rel)) {
+                    if (_service.IsValid() == true) {
+                        _service->Unsubscribe(*this);
+                        _service->Detach(*this);
+                        _service.Release();
+                    }
+                }
+            }
+
             Server& _parent;
             PluginHost::ISecurity* _security;
             Core::ProxyType<Service> _service;
             bool _requestClose;
             Jobs _jobs;
+            std::atomic<bool> _serviceCleanedUp{false};
 
             // Factories for creating jobs that can be placed on the PluginHost Worker pool.
             static Core::ProxyPoolType<WebRequestJob> _webJobs;
