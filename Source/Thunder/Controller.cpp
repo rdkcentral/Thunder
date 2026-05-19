7f6e303cb5885e7ea37d7792c891ad469e976f57
nxtumUbun <mrtymoteusz@gmail.com>
2026-05-19 08:43:12 -0700

    remove static cast register

--- Source/Thunder/Controller.cpp
+++ Source/Thunder/Controller.cpp
@@ -133,7 +133,7 @@ namespace Plugin {
             }
         }
 
-        _service->Register(static_cast<PluginHost::IPlugin::INotification*>(&_systemInfoReport));
+        _service->Register(&_systemInfoReport);
 
         if (config.Ui.Value() == true) {
             _service->EnableWebServer(_T("UI"), EMPTY_STRING);
@@ -180,7 +180,7 @@ namespace Plugin {
             _probe = nullptr;
         }
 
-        _service->Unregister(static_cast<PluginHost::IPlugin::INotification*>(&_systemInfoReport));
+        _service->Unregister(&_systemInfoReport);
 
         /* stop the file serving over http.... */
         service->DisableWebServer();
