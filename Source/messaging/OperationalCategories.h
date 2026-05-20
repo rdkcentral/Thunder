8cda605cebe4b1c57fd9d78301e8d5719912841b
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2026-05-21 01:35:58 +0200

    [R4_4][Messaging] Backporting (#2114)
    * Backport messaging
    
    * Adjust the formatting
    
    * Make the ASSERTs match master after the investigation is over
    
    * Bring back the DataSize static not to break BC
    
    * Make std::out and std::err redirects disabled by default
    
    * Add a way to return controls for all modules for backwards compatibility

---
+++ Source/messaging/OperationalCategories.h
@@ -0,0 +1,80 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2022 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Module.h"
+#include "Control.h"
+
+#define DEFINE_OPERATIONAL_CATEGORY(CATEGORY) \
+    DEFINE_MESSAGING_CATEGORY(WPEFramework::OperationalStream::BaseOperationalType<CATEGORY>, CATEGORY)
+
+#define OPERATIONAL_STREAM_ANNOUNCE(CATEGORY) \
+    WPEFramework::OperationalStream::BaseOperationalType<CATEGORY>::Instance();
+
+namespace WPEFramework {
+
+namespace OperationalStream {
+
+    template <typename CATEGORY>
+    class BaseOperationalType : public Core::Messaging::BaseCategoryType<Core::Messaging::Metadata::type::OPERATIONAL_STREAM> {
+    public:
+        using BaseClass = Core::Messaging::BaseCategoryType<Core::Messaging::Metadata::type::OPERATIONAL_STREAM>;
+        using Control = Messaging::ControlType<CATEGORY, &Core::Messaging::MODULE_OPERATIONAL_STREAM, Core::Messaging::Metadata::type::OPERATIONAL_STREAM>;
+
+        BaseOperationalType(const BaseOperationalType&) = delete;
+        BaseOperationalType& operator=(const BaseOperationalType&) = delete;
+
+        BaseOperationalType() = default;
+        ~BaseOperationalType() = default;
+
+        using BaseClass::BaseClass;
+
+    public:
+        inline static void Announce() {
+            IsEnabled();
+        }
+
+        static Control& Instance() {
+            static Control control(true);
+            return (control);
+        }
+
+        inline static bool IsEnabled() {
+            return (Instance().IsEnabled());
+        }
+
+        inline static Core::Messaging::OutputMode Routing() {
+            return (Instance().Routing());
+        }
+
+        inline static void Enable(const bool enable) {
+            Instance().Enable(enable);
+        }
+        
+        inline static const Core::Messaging::Metadata& Metadata() {
+            return (Instance().Metadata());
+        }
+    };
+
+    DEFINE_OPERATIONAL_CATEGORY(StandardOut)
+    DEFINE_OPERATIONAL_CATEGORY(StandardError)
+
+}
+}
