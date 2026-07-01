24be6455d2bdbc26e9e5fdeea3e812c65168dddc
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2026-07-01 16:48:44 -0400

    Update JSONRPCLink.h

--- Source/websocket/JSONRPCLink.h
+++ Source/websocket/JSONRPCLink.h
@@ -23,12 +23,12 @@
 #include "WebSocketLink.h"
 
 // RDK OpenTelemetry trace context propagation
-#if __has_include("rdk_otlp_instrumentation.h")
-#include "rdk_otlp_instrumentation.h"
-#define RDK_OTEL_THUNDER_ENABLED 1
-#else
-#define RDK_OTEL_THUNDER_ENABLED 0
-#endif
+//#if __has_include("rdk_otlp_instrumentation.h")
+//#include "rdk_otlp_instrumentation.h"
+//#define RDK_OTEL_THUNDER_ENABLED 1
+//#else
+//#define RDK_OTEL_THUNDER_ENABLED 0
+//#endif
 
 namespace WPEFramework {
 
