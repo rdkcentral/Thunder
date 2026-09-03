21352a395c616b26022ac9f04c2f9e8b43889390
nxtum <mrtymoteusz@gmail.com>
2026-09-03 11:23:46 +0000

    enum to string

--- Source/Thunder/Controller.cpp
+++ Source/Thunder/Controller.cpp
@@ -371,8 +371,8 @@ namespace Plugin {
 
         if (attribute.IsSet() == false) {
             Core::JSON::VariantContainer attributes;
-            attributes.Set(_T("prefix"), Core::JSON::Variant(configuration.Prefix()));
-            attributes.Set(_T("idletime"), Core::JSON::Variant(static_cast<uint32_t>(configuration.IdleTime())));
+            attributes.Set(Core::EnumToString(IConfiguration::PREFIX).c_str(), Core::JSON::Variant(configuration.Prefix()));
+            attributes.Set(Core::EnumToString(IConfiguration::IDLETIME).c_str(), Core::JSON::Variant(static_cast<uint32_t>(configuration.IdleTime())));
             attributes.ToString(value);
 
             result = Core::ERROR_NONE;
