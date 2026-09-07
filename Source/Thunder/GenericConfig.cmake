e1ede00b84366352811d570e1737e67938a2610a
nxtum <mrtymoteusz@gmail.com>
2026-09-07 08:33:40 +0000

    Use a generic hibernation socket path

--- Source/Thunder/GenericConfig.cmake
+++ Source/Thunder/GenericConfig.cmake
@@ -51,9 +51,10 @@ set(ETHERNETCARD_NAME "eth0" CACHE STRING "Ethernet Card name which has to be as
 set(GROUP "" CACHE STRING "Define which system group will be used")
 set(UMASK "" CACHE STRING "Set the permission mask for the creation of new files. e.g. 0760")
 set(COMMUNICATOR "" CACHE STRING "Define the ComRPC socket e.g. 127.0.0.1:62000 or /tmp/communicator|750")
-set(LOCATOR "/tmp/memcrcom" CACHE STRING "Default Memecr Socket path")
+set(LOCATOR "/tmp/examplehibernator" CACHE PATH "Default hibernation service socket path")
 set(DISABLEPLUGINAUTOACTIVATION false CACHE STRING "Disable plugin auto activation (override plugin startmode)")
 set(AUTHORIZEDEXTENSIONS "*" CACHE STRING "List all authorized Thunder extensions, use * to allow all extensions, also determines order of Thunder extension activation/deactivation")
+
 
 # Controller Plugin Settings.
 set(PLUGIN_CONTROLLER_UI_ENABLED "true" CACHE STRING "Enable the Controller's UI")
