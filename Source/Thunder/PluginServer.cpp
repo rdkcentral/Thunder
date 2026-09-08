dcb181a9df55f21647ac49c78a63dc1a7f19a63b
sebaszm <45654185+sebaszm@users.noreply.github.com>
2026-09-08 13:04:12 +0200

    [Thunder] Make hibernate wakeup timeout configurable (#2221)
    * [Thunder] Make hibernate wakup timeout confgurable
    
    * typo
    
    * don't use an enum
    
    * stray s

--- Source/Thunder/PluginServer.cpp
+++ Source/Thunder/PluginServer.cpp
@@ -348,7 +348,7 @@ namespace PluginHost {
             Unlock();
             result = Core::ERROR_ILLEGAL_STATE;
         } else if (currentState == IShell::state::HIBERNATED) {
-            result = Wakeup(3000);
+            result = Wakeup();
             Unlock();
         } else if ((currentState == IShell::state::DEACTIVATED) || (currentState == IShell::state::PRECONDITION)) {
 
@@ -549,7 +549,7 @@ namespace PluginHost {
 
             if(currentState == IShell::state::HIBERNATED)
             {
-                uint32_t wakeupResult = Wakeup(3000);
+                uint32_t wakeupResult = Wakeup();
                 if(wakeupResult != Core::ERROR_NONE)
                 {
                     //Force Activated state
