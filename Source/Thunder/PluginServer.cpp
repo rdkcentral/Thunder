2021da48f9092481924c68d7d958d860cc70aaeb
nxtum <94901881+nxtum@users.noreply.github.com>
2026-09-21 12:44:21 +0200

    Introduce runtime configurable framework attributes (#2216)
    * idletime, prefix
    
    * attributes
    
    * Change to Enum attributes
    
    * enum to string
    
    * use enumtocstring
    
    * two copy
    
    * only save pluginhost on explicit
    
    * remove set pendings
    
    * seperate loading

--- Source/Thunder/PluginServer.cpp
+++ Source/Thunder/PluginServer.cpp
@@ -1249,6 +1249,12 @@ namespace PluginHost {
         // Lets assign a workerpool, we created it...
         Core::WorkerPool::Assign(&_dispatcher);
 
+        Override overrides(_config, _services, configuration.PersistentPath() + PluginOverrideDirectory);
+        const uint32_t hostResult = overrides.LoadPluginHost();
+        if (hostResult != Core::ERROR_NONE) {
+            SYSLOG(Logging::Startup, (_T("Loading PluginHost overrides failed. Error [%u]"), hostResult));
+        }
+
         Plugin::Config metaDataConfig;
 
         metaDataConfig.ClassName = Core::ClassNameOnly(typeid(Plugin::Controller).name()).Text();
@@ -1267,7 +1273,10 @@ namespace PluginHost {
         }
 
         // Get the configuration from the persistent location.
-        Load();
+        const uint32_t pluginResult = overrides.Load();
+        if (pluginResult != Core::ERROR_NONE) {
+            SYSLOG(Logging::Startup, (_T("Loading plugin overrides failed. Error [%u]"), pluginResult));
+        }
 
         // Create input handle
         _inputHandler.Initialize(
